a1b4d190ab6b574f93225d0701c20139f8f0c705
Matt Young <dev@mttyng.com>
2018-09-24 11:13:53 -0700

    Remove calls to the mk macro on final returns in parser.rs

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -5,7 +5,8 @@ use std::fs::read_to_string;
 
 pub fn main() {
     let buffer = read_to_string("tests/js/defineVar.js").unwrap();
-    let lexer = Lexer::new(&buffer);
+    let mut lexer = Lexer::new(&buffer);
+    lexer.lex().unwrap();
     let tokens = lexer.tokens;
     match Parser::new(tokens).parse_all() {
         Ok(e) => println!("{}", e),
