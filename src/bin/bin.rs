b73706aab107de3640f3579fb7fc811548f6f0b7
Jason Williams <jase.williams@gmail.com>
2018-11-21 00:31:20 +0000

    change js file

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -5,7 +5,7 @@ use boa::syntax::parser::Parser;
 use std::fs::read_to_string;
 
 pub fn main() {
-    let buffer = read_to_string("tests/js/defineVar.js").unwrap();
+    let buffer = read_to_string("tests/js/test.js").unwrap();
     let mut lexer = Lexer::new(&buffer);
     lexer.lex().unwrap();
     let tokens = lexer.tokens;
