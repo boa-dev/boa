50ffaff0a7abffb04fb54f806939dd2bd8ff0064
Jason Williams <jase.williams@gmail.com>
2019-04-08 23:52:35 +0100

    started work on environment records

--- src/lib/exec.rs
+++ src/lib/exec.rs
@@ -1,3 +1,4 @@
+use crate::environment_record::EnvironmentRecordTrait;
 use crate::js::function::{Function, RegularFunction};
 use crate::js::object::{INSTANCE_PROTOTYPE, PROTOTYPE};
 use crate::js::value::{from_value, to_value, ResultValue, Value, ValueData};
--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,11 +1,12 @@
+extern crate chrono;
 extern crate gc;
 extern crate rand;
 extern crate serde_json;
-extern crate chrono;
 
 #[macro_use]
 extern crate gc_derive;
 
+pub mod environment_record;
 pub mod exec;
 pub mod js;
 pub mod syntax;
