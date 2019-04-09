55b4191fd598157d8dd54787e641e3770e77d292
Jason Williams <jason.williams01@bbc.co.uk>
2019-04-09 17:31:45 +0100

    updating environment_record

--- src/lib/environment_record.rs
+++ src/lib/environment_record.rs
@@ -14,7 +14,7 @@ struct DeclaritiveEnvironmentRecord {
 
 /// https://tc39.github.io/ecma262/#sec-environment-records
 ///
-/// In the ECMAScript specification EnvironmentRecords are hierachical and have a base class with abstract methods.   
+/// In the ECMAScript specification Environment Records are hierachical and have a base class with abstract methods.   
 /// In this implementation we have a trait which represents the behaviour of all EnvironmentRecord types.
 pub trait EnvironmentRecordTrait {
     /// Determine if an Environment Record has a binding for the String value N. Return true if it does and false if it does not.
@@ -41,5 +41,27 @@ pub trait EnvironmentRecordTrait {
     /// If `strict` is true and the binding cannot be set throw a TypeError exception.
     fn set_mutable_binding(&self, name: String, value: Value, strict: bool);
 
+    /// Returns the value of an already existing binding from an Environment Record.
+    /// The String value N is the text of the bound name.
+    /// S is used to identify references originating in strict mode code or that
+    /// otherwise require strict mode reference semantics.
     fn get_binding_value(&self, name: String, strict: bool) -> Value;
+
+    /// Delete a binding from an Environment Record.
+    /// The String value name is the text of the bound name.
+    /// If a binding for name exists, remove the binding and return true.
+    /// If the binding exists but cannot be removed return false. If the binding does not exist return true.
+    fn delete_binding(&self, name: String) -> bool;
+
+    /// Determine if an Environment Record establishes a this binding.
+    /// Return true if it does and false if it does not.
+    fn has_this_binding(&self) -> bool;
+
+    /// Determine if an Environment Record establishes a super method binding.
+    /// Return true if it does and false if it does not.
+    fn has_super_binding(&self) -> bool;
+
+    /// If this Environment Record is associated with a with statement, return the with object.
+    /// Otherwise, return undefined.
+    fn with_base_object(&self) -> Value;
 }
