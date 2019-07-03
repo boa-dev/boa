dd0129e8864aefd1c9532b322fcadb3b6a54dc25
Callum Ward <wards.callum@gmail.com>
2019-07-04 00:22:53 +0100

    Implement push, pop and join

--- src/lib/js/array.rs
+++ src/lib/js/array.rs
@@ -22,6 +22,21 @@ fn create_array_object(array_obj: Value, array_contents: Vec<Value>) -> ResultVa
     Ok(array_obj_ptr)
 }
 
+/// Utility function which takes an existing array object and puts additional
+/// values on the end, correctly rewriting the length
+fn add_to_array_object(array_ptr: Value, add_values: Vec<Value>) -> ResultValue {
+    let orig_length: i32 = from_value(array_ptr.get_field_slice("length")).unwrap();
+
+    for (n, value) in add_values.iter().enumerate() {
+        let new_index = orig_length + (n as i32);
+        array_ptr.set_field(new_index.to_string(), value.clone());
+    }
+
+    array_ptr.set_field_slice("length", to_value(orig_length + add_values.len() as i32));
+
+    Ok(array_ptr)
+}
+
 /// Create a new array
 pub fn make_array(this: Value, _: Value, args: Vec<Value>) -> ResultValue {
     // Make a new Object which will internally represent the Array (mapping
@@ -77,6 +92,57 @@ pub fn concat(this: Value, _: Value, args: Vec<Value>) -> ResultValue {
     create_array_object(this, new_values)
 }
 
+/// Array.prototype.push ( ...items )
+///
+/// The arguments are appended to the end of the array, in the order in which
+/// they appear. The new length of the array is returned as the result of the
+/// call.
+pub fn push(this: Value, _: Value, args: Vec<Value>) -> ResultValue {
+    let new_array = add_to_array_object(this, args)?;
+    Ok(new_array.get_field_slice("length"))
+}
+
+/// Array.prototype.pop ( )
+///
+/// The last element of the array is removed from the array and returned.
+pub fn pop(this: Value, _: Value, _: Vec<Value>) -> ResultValue {
+    let curr_length: i32 = from_value(this.get_field_slice("length")).unwrap();
+    if curr_length < 1 {
+        return Err(to_value(
+            "Cannot pop() on an array with zero length".to_string(),
+        ));
+    }
+    let pop_index = curr_length - 1;
+    let pop_value: Value = this.get_field(pop_index.to_string());
+    this.remove_prop(&pop_index.to_string());
+    this.set_field_slice("length", to_value(pop_index));
+    Ok(pop_value)
+}
+
+/// Array.prototype.join ( separator )
+///
+/// The elements of the array are converted to Strings, and these Strings are
+/// then concatenated, separated by occurrences of the separator. If no
+/// separator is provided, a single comma is used as the separator.
+pub fn join(this: Value, _: Value, args: Vec<Value>) -> ResultValue {
+    let separator: String;
+
+    if args.len() > 0 {
+        separator = args[0].to_string();
+    } else {
+        separator = ",".to_string();
+    }
+
+    let mut elem_strs: Vec<String> = Vec::new();
+    let length: i32 = from_value(this.get_field_slice("length")).unwrap();
+    for n in 0..length {
+        let elem_str: String = this.get_field(n.to_string()).to_string();
+        elem_strs.push(elem_str);
+    }
+
+    Ok(to_value(elem_strs.join(&separator)))
+}
+
 /// Create a new `Array` object
 pub fn _create(global: &Value) -> Value {
     let array = to_value(make_array as NativeFunctionData);
@@ -90,7 +156,14 @@ pub fn _create(global: &Value) -> Value {
         set: Gc::new(ValueData::Undefined),
     };
     proto.set_prop_slice("length", length);
-    proto.set_field_slice("concat", to_value(concat as NativeFunctionData));
+    let concat_func = to_value(concat as NativeFunctionData);
+    concat_func.set_field_slice("length", to_value(1 as i32));
+    proto.set_field_slice("concat", concat_func);
+    let push_func = to_value(push as NativeFunctionData);
+    push_func.set_field_slice("length", to_value(1 as i32));
+    proto.set_field_slice("push", push_func);
+    proto.set_field_slice("pop", to_value(pop as NativeFunctionData));
+    proto.set_field_slice("join", to_value(join as NativeFunctionData));
     array.set_field_slice(PROTOTYPE, proto);
     array
 }
