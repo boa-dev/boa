c07ad300578c571f3ddcce34237df1cac691b9ae
Jason Williams <jwilliams720@bloomberg.net>
2019-07-28 20:46:24 +0100

    comments on becnhmark

--- benches/string.rs
+++ benches/string.rs
@@ -13,12 +13,15 @@ fn hello_world_lexer(c: &mut Criterion) {
     c.bench_function("Hello World (Lexer)", move |b| {
         b.iter(|| {
             let mut lexer = Lexer::new(black_box(SRC));
+            // return the value into the blackbox so its not optimized away
+            // https://gist.github.com/jasonwilliams/5325da61a794d8211dcab846d466c4fd
             lexer.lex()
         })
     });
 }
 
 fn hello_world_parser(c: &mut Criterion) {
+    // Don't include lexing as part of the parser benchmark
     let mut lexer = Lexer::new(SRC);
     lexer.lex().expect("failed to lex");
     let tokens = lexer.tokens;
