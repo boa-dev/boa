7ca418dc05c3024363c3c94688af52706cb018ea
Jason Williams <jwilliams720@bloomberg.net>
2019-07-28 20:13:23 +0100

    fixing #84

--- benches/string.rs
+++ benches/string.rs
@@ -13,20 +13,20 @@ fn hello_world_lexer(c: &mut Criterion) {
     c.bench_function("Hello World (Lexer)", move |b| {
         b.iter(|| {
             let mut lexer = Lexer::new(black_box(SRC));
-            let _ = lexer.lex();
+            lexer.lex()
         })
     });
 }
 
 fn hello_world_parser(c: &mut Criterion) {
-    c.bench_function("Hello World (Parser)", move |b| {
+    let mut lexer = Lexer::new(SRC);
+    lexer.lex().expect("failed to lex");
+    let tokens = lexer.tokens;
+    c.bench_function_over_inputs("Hello World (Parser)", move |b, tok| {
         b.iter(|| {
-            let mut lexer = Lexer::new(SRC);
-            lexer.lex().expect("failed to lex");
-            let tokens = lexer.tokens;
-            Parser::new(black_box(tokens)).parse_all().unwrap();
+            Parser::new(black_box(tok.to_vec())).parse_all().unwrap();
         })
-    });
+    }, vec![tokens]);
 }
 
 fn hello_world(c: &mut Criterion) {
