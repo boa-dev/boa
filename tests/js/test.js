73d158cce2368d30cc3e0466e8ff4e3f5099e565
Jason Williams <936006+jasonwilliams@users.noreply.github.com>
2019-09-18 22:57:55 +0100

    testing small change (#104)

--- tests/js/test.js
+++ tests/js/test.js
@@ -1,12 +1,2 @@
-let num = 12;
-
-function fib(n) {
-  if (n <= 1) return 1;
-  return fib(n - 1) + fib(n - 2);
-}
-
-let res = fib(num);
-
-res;
-
-// (2 - 1 = 1) + (2 - 2 = rt 1)
+let a = "hello world";
+a;
