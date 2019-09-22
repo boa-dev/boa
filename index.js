4b7b331c6ea53a73dbe2a22a1ae68d16352e57f7
Jason Williams <jase.williams@gmail.com>
2019-09-22 16:48:05 +0100

    fixes #99

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Note that a dynamic `import` statement here is required due to
 // webpack/webpack#6615, but in theory `import { greet } from './pkg/hello_world';`
 // will work here one day as well!
-const rust = import("./pkg/boa");
+const rust = import("./pkg");
 // const image = import("./assets/01_rust_loves_js.png");
 
 rust.then(m => {
