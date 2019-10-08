5246d0b5c555da40f37b7947d117f028b38f1e4e
Ojas Landge <ojaslandge@gmail.com>
2019-10-08 16:31:36 -0500

    Use Monaco Editor for demo.

--- index.js
+++ index.js
@@ -2,16 +2,33 @@
 // webpack/webpack#6615, but in theory `import { greet } from './pkg/hello_world';`
 // will work here one day as well!
 const rust = import("./pkg");
+import * as monaco from "monaco-editor";
 // const image = import("./assets/01_rust_loves_js.png");
+
+const editor = monaco.editor.create(
+  document.getElementsByClassName("textbox")[0], 
+  {
+    value: "",
+    language: "javascript",
+    theme: "vs-dark"
+  }
+);
+
+
+// Fix size of Monaco Editor when window resize
+window.addEventListener('resize', () => {
+  editor.layout();
+});
+
 
 rust.then(m => {
   window.evaluate = m.evaluate;
-  let textarea = document.querySelector("textarea");
-  textarea.addEventListener("input", inputHandler);
+
+  editor.getModel().onDidChangeContent(inputHandler);
 });
 
 function inputHandler(evt) {
-  let text = evt.target.value;
+  const text = editor.getValue();
   let p = document.querySelector("p.output");
   let t0 = performance.now();
   let result = window.evaluate(text);
