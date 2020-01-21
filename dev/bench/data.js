167af5c351d9f07ca15450ca3de7b844ffad0ead
github-action-benchmark <github@users.noreply.github.com>
2020-01-21 00:03:44 +0000

    add Boa Benchmarks (criterion) benchmark result for 495f0a48686b362613b0befc8a6e8a91563a81f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1579563059694,
+  "lastUpdate": 1579565024361,
   "repoUrl": "https://github.com/jasonwilliams/boa",
   "entries": {
     "Boa Benchmarks": [
@@ -65,6 +65,72 @@ window.BENCHMARK_DATA = {
             "name": "Hello World (Parser)",
             "value": 1.3541,
             "range": "+/- 0.026",
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "936006+jasonwilliams@users.noreply.github.com",
+            "name": "Jason Williams",
+            "username": "jasonwilliams"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "495f0a48686b362613b0befc8a6e8a91563a81f6",
+          "message": "String.prototype.replace() (#217)\n\n* String Replace addition\r\n\r\n* Function argument now fully implemented\r\n\r\n* adding substitutions\r\n\r\n* finish off String.prototype.replace\r\n\r\n* use is_some()\r\n\r\n* fixing string\r\n\r\n* clippy",
+          "timestamp": "2020-01-20T23:57:18Z",
+          "tree_id": "92f8653a94efe6bcd11c24d67566851a703e8fdf",
+          "url": "https://github.com/jasonwilliams/boa/commit/495f0a48686b362613b0befc8a6e8a91563a81f6"
+        },
+        "date": 1579565022755,
+        "tool": "criterion",
+        "benches": [
+          {
+            "name": "Create Realm",
+            "value": 414.6,
+            "range": "+/- 6.690",
+            "unit": "us"
+          },
+          {
+            "name": "Symbol Creation",
+            "value": 501.42,
+            "range": "+/- 8.400",
+            "unit": "us"
+          },
+          {
+            "name": "fibonacci (Execution)",
+            "value": 5.1035,
+            "range": "+/- 0.067",
+            "unit": "ms"
+          },
+          {
+            "name": "Expression (Parser)",
+            "value": 9.5606,
+            "range": "+/- 0.145",
+            "unit": "us"
+          },
+          {
+            "name": "Hello World (Execution)",
+            "value": 434.01,
+            "range": "+/- 7.200",
+            "unit": "us"
+          },
+          {
+            "name": "Hello World (Lexer)",
+            "value": 1.0516,
+            "range": "+/- 0.017",
+            "unit": "us"
+          },
+          {
+            "name": "Hello World (Parser)",
+            "value": 1.0471,
+            "range": "+/- 0.018",
             "unit": "us"
           }
         ]
