498febf5b4e8475a6284aa134ef1f7de5cf0116b
John Doneth <doneth7@gmail.com>
2020-03-03 11:00:25 -0500

    Add basic REPL functionality

--- boa_cli/src/main.rs
+++ boa_cli/src/main.rs
@@ -3,6 +3,7 @@
 #![allow(clippy::cognitive_complexity)]
 
 use boa::{exec::Executor, forward_val, realm::Realm};
+use std::io;
 use std::{fs::read_to_string, path::PathBuf};
 use structopt::StructOpt;
 
@@ -11,7 +12,7 @@ use structopt::StructOpt;
 #[structopt(author, about)]
 struct Opt {
     /// The javascript file to be evaluated.
-    #[structopt(name = "FILE", parse(from_os_str), default_value = "tests/js/test.js")]
+    #[structopt(name = "FILE", parse(from_os_str))]
     files: Vec<PathBuf>,
     /// Open a boa shell (WIP).
     #[structopt(short, long)]
@@ -24,12 +25,25 @@ pub fn main() -> Result<(), std::io::Error> {
     let realm = Realm::create();
     let mut engine = Executor::new(realm);
 
-    for file in args.files {
-        let buffer = read_to_string(file)?;
+    if args.shell || args.files.is_empty() {
+        loop {
+            let mut buffer = String::new();
 
-        match forward_val(&mut engine, &buffer) {
-            Ok(v) => print!("{}", v.to_string()),
-            Err(v) => eprint!("{}", v.to_string()),
+            io::stdin().read_line(&mut buffer)?;
+
+            match forward_val(&mut engine, buffer.trim_end()) {
+                Ok(v) => println!("{}", v.to_string()),
+                Err(v) => eprintln!("{}", v.to_string()),
+            }
+        }
+    } else {
+        for file in args.files {
+            let buffer = read_to_string(file)?;
+
+            match forward_val(&mut engine, &buffer) {
+                Ok(v) => print!("{}", v.to_string()),
+                Err(v) => eprint!("{}", v.to_string()),
+            }
         }
     }
 
