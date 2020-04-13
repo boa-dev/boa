51e047fa32bae4da14412bc57b7631f32666c997
Iban Eguia <razican@protonmail.ch>
2020-04-13 22:09:14 +0200

    Update boa/src/syntax/parser/tests.rs

--- boa/src/syntax/parser/tests.rs
+++ boa/src/syntax/parser/tests.rs
@@ -725,6 +725,7 @@ fn check_do_while() {
             Box::new(Node::Const(Const::Bool(true))),
         )],
     );
+}
 
 /// Should be parsed as `new Class().method()` instead of `new (Class().method())`
 #[test]
