854c96f0c98fe9a0df163b058d044d1af08e4a3e
Noah <33094578+coolreader18@users.noreply.github.com>
2020-04-19 00:37:56 -0500

    Fix Value.to_json and add JSON tests

--- boa/src/builtins/json.rs
+++ boa/src/builtins/json.rs
@@ -3,7 +3,7 @@ use crate::builtins::function::NativeFunctionData;
 /// <https://tc39.es/ecma262/#sec-json-object>
 use crate::builtins::value::{to_value, ResultValue, Value, ValueData};
 use crate::exec::Interpreter;
-use serde_json::{self, to_string_pretty, Value as JSONValue};
+use serde_json::{self, Value as JSONValue};
 
 /// Parse a JSON string into a Javascript object
 /// <https://tc39.es/ecma262/#sec-json.parse>
@@ -22,8 +22,8 @@ pub fn parse(_: &Value, args: &[Value], _: &mut Interpreter) -> ResultValue {
 /// Process a Javascript object into a JSON string
 pub fn stringify(_: &Value, args: &[Value], _: &mut Interpreter) -> ResultValue {
     let obj = args.get(0).expect("cannot get argument for JSON.stringify");
-    let json = obj.to_json();
-    Ok(to_value(to_string_pretty(&json).expect("")))
+    let json: String = obj.to_json().to_string();
+    Ok(to_value(json))
 }
 
 /// Create a new `JSON` object
@@ -34,4 +34,27 @@ pub fn create_constructor(global: &Value) -> Value {
     make_builtin_fn!(stringify, named "stringify", with length 3, of json);
 
     to_value(json)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::exec::Executor;
+    use crate::forward;
+    use crate::realm::Realm;
+    #[test]
+    fn json_sanity() {
+        let realm = Realm::create();
+        let mut engine = Executor::new(realm);
+        assert_eq!(
+            forward(&mut engine, r#"JSON.parse('{"aaa":"bbb"}').aaa == 'bbb'"#),
+            "true"
+        );
+        assert_eq!(
+            forward(
+                &mut engine,
+                r#"JSON.stringify({aaa: 'bbb'}) == '{"aaa":"bbb"}'"#
+            ),
+            "true"
+        );
+    }
 }
