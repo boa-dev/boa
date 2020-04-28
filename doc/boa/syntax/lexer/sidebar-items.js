fe3eded61963b80ac52c757855b8b044f9fa3c51
HalidOdat <HalidOdat@users.noreply.github.com>
2020-04-28 23:38:42 +0000

    Deploy to GitHub pages

--- doc/boa/syntax/lexer/sidebar-items.js
+++ doc/boa/syntax/lexer/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Lexer","A lexical analyzer for JavaScript source code"],["LexerError","An error that occurred during lexing or compiling of the source input."]]});
+initSidebarItems({"struct":[["Lexer","A lexical analyzer for JavaScript source code."],["LexerError","An error that occurred during lexing or compiling of the source input."]]});
