402041d43251017ab772fe6f9920e41f496a359d
hello2dj <dj_amazing@sina.com>
2020-05-14 03:01:42 +0800

    impl abstract-equality-comparison (#395)

--- boa/src/builtins/value/operations.rs
+++ boa/src/builtins/value/operations.rs
@@ -1,23 +1,63 @@
 use super::*;
+use crate::builtins::number;
+use crate::Interpreter;
 
-impl PartialEq for Value {
-    fn eq(&self, other: &Self) -> bool {
+use std::borrow::Borrow;
+
+impl Value {
+    /// Strict equality comparison.
+    ///
+    /// This method is executed when doing strict equality comparisons with the `===` operator.
+    /// For more information, check <https://tc39.es/ecma262/#sec-strict-equality-comparison>.
+    pub fn strict_equals(&self, other: &Self) -> bool {
+        if self.get_type() != other.get_type() {
+            return false;
+        }
+
+        if self.is_number() {
+            return number::equals(f64::from(self), f64::from(other));
+        }
+
+        same_value_non_number(self, other)
+    }
+
+    /// Abstract equality comparison.
+    ///
+    /// This method is executed when doing abstract equality comparisons with the `==` operator.
+    ///  For more information, check <https://tc39.es/ecma262/#sec-abstract-equality-comparison>
+    pub fn equals(&mut self, other: &mut Self, interpreter: &mut Interpreter) -> bool {
+        if self.get_type() == other.get_type() {
+            return self.strict_equals(other);
+        }
+
         match (self.data(), other.data()) {
-            // TODO: fix this
-            // _ if self.ptr.to_inner() == &other.ptr.to_inner() => true,
             _ if self.is_null_or_undefined() && other.is_null_or_undefined() => true,
-            (ValueData::String(_), _) | (_, ValueData::String(_)) => {
-                self.to_string() == other.to_string()
+
+            // https://github.com/rust-lang/rust/issues/54883
+            (ValueData::Integer(_), ValueData::String(_))
+            | (ValueData::Rational(_), ValueData::String(_))
+            | (ValueData::String(_), ValueData::Integer(_))
+            | (ValueData::String(_), ValueData::Rational(_))
+            | (ValueData::Rational(_), ValueData::Boolean(_))
+            | (ValueData::Integer(_), ValueData::Boolean(_)) => {
+                let a: &Value = self.borrow();
+                let b: &Value = other.borrow();
+                number::equals(f64::from(a), f64::from(b))
             }
-            (ValueData::Boolean(a), ValueData::Boolean(b)) if a == b => true,
-            (ValueData::Rational(a), ValueData::Rational(b))
-                if a == b && !a.is_nan() && !b.is_nan() =>
-            {
-                true
+            (ValueData::Boolean(_), _) => {
+                other.equals(&mut Value::from(self.to_integer()), interpreter)
             }
-            (ValueData::Rational(a), _) if *a == other.to_number() => true,
-            (_, ValueData::Rational(a)) if *a == self.to_number() => true,
-            (ValueData::Integer(a), ValueData::Integer(b)) if a == b => true,
+            (_, ValueData::Boolean(_)) => {
+                self.equals(&mut Value::from(other.to_integer()), interpreter)
+            }
+            (ValueData::Object(_), _) => {
+                let mut primitive = interpreter.to_primitive(self, None);
+                primitive.equals(other, interpreter)
+            }
+            (_, ValueData::Object(_)) => {
+                let mut primitive = interpreter.to_primitive(other, None);
+                primitive.equals(self, interpreter)
+            }
             _ => false,
         }
     }
@@ -96,6 +136,25 @@ impl Not for Value {
     }
 }
 
+/// The internal comparison abstract operation SameValueZero(x, y),
+/// where x and y are ECMAScript language values, produces true or false.
+/// SameValueZero differs from SameValue only in its treatment of +0 and -0.
+///
+/// Such a comparison is performed as follows:
+///
+/// <https://tc39.es/ecma262/#sec-samevaluezero>
+pub fn same_value_zero(x: &Value, y: &Value) -> bool {
+    if x.get_type() != y.get_type() {
+        return false;
+    }
+
+    if x.is_number() {
+        return number::same_value_zero(f64::from(x), f64::from(y));
+    }
+
+    same_value_non_number(x, y)
+}
+
 /// The internal comparison abstract operation SameValue(x, y),
 /// where x and y are ECMAScript language values, produces true or false.
 /// Such a comparison is performed as follows:
@@ -114,10 +173,10 @@ pub fn same_value(x: &Value, y: &Value, strict: bool) -> bool {
         return false;
     }
 
-    if x.get_type() == "number" {
-        let native_x = f64::from(x);
-        let native_y = f64::from(y);
-        return native_x.abs() - native_y.abs() == 0.0;
+    // TODO: check BigInt
+    // https://github.com/jasonwilliams/boa/pull/358
+    if x.is_number() {
+        return number::same_value(f64::from(x), f64::from(y));
     }
 
     same_value_non_number(x, y)
@@ -135,7 +194,7 @@ pub fn same_value_non_number(x: &Value, y: &Value) -> bool {
             false
         }
         "boolean" => bool::from(x) == bool::from(y),
-        "object" => *x == *y,
+        "object" => std::ptr::eq(x, y),
         _ => false,
     }
 }
