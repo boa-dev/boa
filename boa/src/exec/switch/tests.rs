5fed339e0980f37bf44f921b30461445a19df794
Paul Lancaster <paul@lancasterzone.com>
2020-06-03 17:24:58 +0100

    Further tests

--- boa/src/exec/switch/tests.rs
+++ boa/src/exec/switch/tests.rs
@@ -78,3 +78,61 @@ fn two_case_no_break_switch() {
     "#;
     assert_eq!(&exec(scenario), "300");
 }
+
+#[test]
+fn three_case_partial_fallthrough() {
+    let scenario = r#"
+        let a = 10;
+        let b = 10;
+
+        switch (a) {
+            case 10:
+                a = 150;
+            case 20:
+                b = 150;
+                break;
+            case 15:
+                b = 1000;
+                break;
+        }
+        
+        a + b;
+    "#;
+    assert_eq!(&exec(scenario), "300");
+}
+
+#[test]
+fn default_taken_switch() {
+    let scenario = r#"
+        let a = 10;
+
+        switch (a) {
+            case 5:
+                a = 150;
+                break;
+            default:
+                a = 70;
+        }
+        
+        a;
+    "#;
+    assert_eq!(&exec(scenario), "70");
+}
+
+#[test]
+fn default_not_taken_switch() {
+    let scenario = r#"
+        let a = 5;
+
+        switch (a) {
+            case 5:
+                a = 150;
+                break;
+            default:
+                a = 70;
+        }
+        
+        a;
+    "#;
+    assert_eq!(&exec(scenario), "150");
+}
