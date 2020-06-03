60a15cf58b51384ac8d7499867ff6fd81b982751
Paul Lancaster <paul@lancasterzone.com>
2020-06-03 17:01:04 +0100

    Simple switch cases tested, lack of break not impl

--- boa/src/exec/switch/tests.rs
+++ boa/src/exec/switch/tests.rs
@@ -14,3 +14,67 @@ fn single_case_switch() {
     "#;
     assert_eq!(&exec(scenario), "20");
 }
+
+#[test]
+fn no_cases_switch() {
+    let scenario = r#"
+        let a = 10;
+        switch (a) {
+        }
+        
+        a;
+    "#;
+    assert_eq!(&exec(scenario), "10");
+}
+
+#[test]
+fn no_true_case_switch() {
+    let scenario = r#"
+        let a = 10;
+        switch (a) {
+            case 5:
+                a = 15;
+                break;
+        }
+        
+        a;
+    "#;
+    assert_eq!(&exec(scenario), "10");
+}
+
+#[test]
+fn two_case_switch() {
+    let scenario = r#"
+        let a = 10;
+        switch (a) {
+            case 5:
+                a = 15;
+                break;
+            case 10:
+                a = 20;
+                break;
+        }
+        
+        a;
+    "#;
+    assert_eq!(&exec(scenario), "20");
+}
+
+#[test]
+fn two_case_no_break_switch() {
+    let scenario = r#"
+        let a = 10;
+        let b = 10;
+
+        switch (a) {
+            case 10:
+                a = 150;
+            case 20:
+                b = 150;
+                break;
+        }
+        
+        a + b;
+    "#;
+    assert_eq!(&exec(scenario), "300");
+}
