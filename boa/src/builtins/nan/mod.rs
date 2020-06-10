14f03b912465a3b40c5869026e86087f4ec2eff6
croraf <vrafaeli@msn.com>
2020-06-10 19:54:42 +0200

    Import test file and fix its linter errors

--- boa/src/builtins/nan/mod.rs
+++ boa/src/builtins/nan/mod.rs
@@ -1,6 +1,8 @@
+#[cfg(test)] mod tests;
+
 use crate::{builtins::value::Value, BoaProfiler};
 
-/// Initialise the `NaN` property on the global object.
+/// Initialize the `NaN` property on the global object.
 #[inline]
 pub fn init(global: &Value) {
     let _timer = BoaProfiler::global().start_event("NaN", "init");
--- boa/src/builtins/nan/tests.rs
+++ boa/src/builtins/nan/tests.rs
@@ -1,4 +1,4 @@
-use crate::{builtins::Value, exec, exec::Interpreter, forward, realm::Realm};
+use crate::exec;
 
 #[test]
 fn nan_exists_on_global_object_and_evaluates_to_nan_value() {
