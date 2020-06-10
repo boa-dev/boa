fb442da6d6fc784fac5ce244783a9cf090700b0d
croraf <vrafaeli@msn.com>
2020-06-10 19:38:10 +0200

    Comment fix

--- boa/src/builtins/nan/mod.rs
+++ boa/src/builtins/nan/mod.rs
@@ -1,6 +1,6 @@
 use crate::{builtins::value::Value, BoaProfiler};
 
-/// Initialise the `Math` object on the global object.
+/// Initialise the `NaN` property on the global object.
 #[inline]
 pub fn init(global: &Value) {
     let _timer = BoaProfiler::global().start_event("NaN", "init");
