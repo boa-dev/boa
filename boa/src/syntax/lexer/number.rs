6dcc784260c1a70f3db584deeb871a0cefa7335f
Paul Lancaster <paul@lancasterzone.com>
2020-06-14 13:25:59 +0100

    More of numeric lexing

--- boa/src/syntax/lexer/number.rs
+++ boa/src/syntax/lexer/number.rs
@@ -1,7 +1,9 @@
 use super::{Cursor, Error, TokenKind, Tokenizer};
+use crate::builtins::BigInt;
 use crate::syntax::ast::{Position, Span};
 use crate::syntax::lexer::{token::Numeric, Token};
 use std::io::Read;
+use std::str::FromStr;
 
 /// Number literal lexing.
 ///
@@ -66,8 +68,10 @@ impl<R> Tokenizer<R> for NumberLiteral {
         // Default assume the number is a base 10 integer.
         let mut kind = NumericKind::Integer(10);
 
+        let c = cursor.next();
+
         if self.init == '0' {
-            match cursor.next() {
+            match c {
                 None => {
                     // DecimalLiteral lexing.
                     // Indicates that the number is just a single 0.
@@ -88,6 +92,7 @@ impl<R> Tokenizer<R> for NumberLiteral {
                     kind = NumericKind::Integer(8);
                 }
                 Some(Ok('b')) | Some(Ok('B')) => {
+                    // BinaryIntegerLiteral
                     kind = NumericKind::Integer(2);
                 }
                 Some(Ok('n')) => {
@@ -109,252 +114,179 @@ impl<R> Tokenizer<R> for NumberLiteral {
                             buf.push(ch);
                             kind = NumericKind::Integer(8);
                         }
-                    }
-                }
-            }
-        }
-
-        unimplemented!();
-    }
-}
-
-/*
-impl<R> Tokenizer<R> for NumberLiteral {
-    fn lex(&mut self, cursor: &mut Cursor<R>, start_pos: Position) -> Result<Token, Error>
-    where
-        R: Read,
-    {
-        /// This is a helper structure
-        ///
-        /// This structure helps with identifying what numerical type it is and what base is it.
-        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-        enum NumericKind {
-            Rational,
-            Integer(u8),
-            BigInt(u8),
-        }
-
-        impl NumericKind {
-            /// Get the base of the number kind.
-            fn base(self) -> u32 {
-                match self {
-                    Self::Rational => 10,
-                    Self::Integer(base) => base as u32,
-                    Self::BigInt(base) => base as u32,
-                }
-            }
-
-            /// Converts `self` to BigInt kind.
-            fn to_bigint(self) -> Self {
-                match self {
-                    Self::Rational => unreachable!("can not convert rational number to BigInt"),
-                    Self::Integer(base) => Self::BigInt(base),
-                    Self::BigInt(base) => Self::BigInt(base),
-                }
-            }
-        }
-
-        // TODO: Setup strict mode.
-        let strict_mode = false;
-
-        let mut buf = self.init.to_string();
-
-        let mut kind = NumericKind::Integer(10);
-
-        if self.init == '0' {
-            match cursor.peek() {
-                None => {
-                    cursor.next_column();
-                    return Ok(Token::new(
-                        TokenKind::NumericLiteral(NumericLiteral::Integer(0)),
-                        Span::new(start_pos, cursor.pos()),
-                    ));
-                }
-                Some(r) => {
-                    match r.map_err(|e| Error::IO(e))? {
-                        'x' | 'X' => {
-                            cursor.next();
-                            cursor.next_column();
-                            kind = NumericKind::Integer(16);
+                    } else if ch.is_digit(36) {
+                        // Indicates a numerical digit comes after then 0 but it isn't an octal digit
+                        // so therefore this must be a number with an unneeded leading 0. This is
+                        // forbidden in strict mode.
+                        if self.strict_mode {
+                            // LegacyOctalIntegerLiteral is forbidden with strict mode true.
+                            return Err(Error::strict(
+                                "Leading 0's are not allowed in strict mode.",
+                            ));
+                        } else {
+                            buf.push(ch);
                         }
-                        'o' | 'O' => {
-                            cursor.next();
-                            cursor.next_column();
-                            kind = NumericKind::Integer(8);
-                        }
-                        'b' | 'B' => {
-                            cursor.next();
-                            cursor.next_column();
-                            kind = NumericKind::Integer(2);
-                        }
-                        ch if ch.is_ascii_digit() => {
-                            let mut is_implicit_octal = true;
-                            while let Some(ch) = cursor.peek(){
-                                let c = ch.map_err(|e| Error::IO(e))?;
-                                if !c.is_ascii_digit() {
-                                    break;
-                                } else if !c.is_digit(8) {
-                                    is_implicit_octal = false;
-                                }
-                                cursor.next();
-                                buf.push(c);
-                            }
-                            if !strict_mode {
-                                if is_implicit_octal {
-                                    kind = NumericKind::Integer(8);
-                                }
-                            } else {
-                                return Err(if is_implicit_octal {
-                                    Error::strict(
-                                        "Implicit octal literals are not allowed in strict mode.",
-                                    )
-                                } else {
-                                    Error::strict(
-                                        "Decimals with leading zeros are not allowed in strict mode.",
-                                    )
-                                });
-                            }
-                        }
-                        _ => {}
+                    } else {
+                        // Indicates that the symbol is a non-number, this is valid if it is a dot or similar.
+                        buf.push(ch);
                     }
                 }
             }
         }
 
         while let Some(ch) = cursor.peek() {
-            let c = ch.map_err(|e| Error::IO(e))?;
-            if !c.is_digit(kind.base()) {
-                break;
+            match ch {
+                Err(_e) => {
+                    // TODO, handle.
+                }
+                Ok(c) if c.is_digit(kind.base()) => {
+                    let s = cursor.next().unwrap().unwrap();
+                    buf.push(s);
+                }
+                _ => {
+                    // A non-number symbol detected, this might be a dot or similar.
+                    break;
+                }
             }
-            cursor.next();
-            buf.push(c);
         }
 
         if cursor.next_is('n')? {
+            // DecimalBigIntegerLiteral
             kind = kind.to_bigint();
         }
 
         if let NumericKind::Integer(10) = kind {
-            'digitloop: while let Some(cx) = cursor.peek() {
-                match cx.map_err(|e| Error::IO(e))? {
-                    '.' => loop {
+            'digitloop: while let Some(ch) = cursor.peek() {
+                match ch {
+                    Err(_e) => {
+                        // TODO
+                    }
+                    Ok('.') => loop {
                         kind = NumericKind::Rational;
-                        cursor.next();
-                        buf.push('.');
-
-                        let c = match cursor.peek() {
-                            Some(ch) => ch.map_err(|e| Error::IO(e))?,
-                            None => break,
-                        };
-
-                        match c {
-                            'e' | 'E' => {
-                                cursor.next(); // Consume 'e' or 'E'
-
-                                match cursor.peek() {
-                                    None => {
-                                        cursor.next();
-                                    }
-                                    Some(x) => {
-                                        let val = x.map_err(|e| Error::IO(e))?;
-                                        match val.to_digit(10) {
-                                            Some(0..=9) => {
-                                                cursor.next(); // Consume digit.
-                                                buf.push(val);
-                                            }
-                                            _ => {
-                                                break 'digitloop;
-                                            }
-                                        }
-                                    }
-                                    _ => {
-                                        break 'digitloop;
-                                    }
-                                }
-                            }
-                            _ => {
-                                if !c.is_digit(10) {
-                                    break 'digitloop;
-                                }
-                            }
-                        }
-                    },
-                    'e' | 'E' => {
-                        cursor.next(); // Consume 'e' or 'E'
-                        kind = NumericKind::Rational;
-                        match cursor.peek() {
+                        match cursor.next() {
                             None => {
-                                cursor.next();
-                            }
-                            Some(x) => {
-                                let val = x.map_err(|e| Error::IO(e))?;
-                                match val.to_digit(10) {
-                                    Some(0..=9) => {
-                                        cursor.next(); // Consume digit.
-                                        buf.push(val);
-                                    }
-                                    _ => {
-                                        break;
-                                    }
-                                }
-                            }
-                            _ => {
+                                // Finished
                                 break;
                             }
+
+                            Some(Err(e)) => {
+                                return Err(Error::from(e));
+                            }
+
+                            Some(Ok(c)) => {
+                                buf.push(c);
+                            }
                         }
+
+                        match cursor.peek() {
+                            None => {
+                                break;
+                            }
+                            Some(Err(_e)) => {
+                                // TODO
+                            }
+                            Some(Ok('e')) | Some(Ok('E')) => {
+                                // TODO scientific notation.
+
+                                unimplemented!();
+
+                                // match self
+                                //     .preview_multiple_next(2)
+                                //     .unwrap_or_default()
+                                //     .to_digit(10)
+                                // {
+                                //     Some(0..=9) | None => {
+                                //         buf.push(self.next());
+                                //     }
+                                //     _ => {
+                                //         break 'digitloop;
+                                //     }
+                                // }
+                            }
+                            Some(Ok(cx)) if !cx.is_digit(10) => {
+                                break 'digitloop;
+                            }
+                            _ => {}
+                        }
+                    },
+                    Ok('e') | Ok('E') => {
+                        // TODO scientific notation.
+                        unimplemented!();
+
+                        // kind = NumericKind::Rational;
+                        // match self
+                        //     .preview_multiple_next(2)
+                        //     .unwrap_or_default()
+                        //     .to_digit(10)
+                        // {
+                        //     Some(0..=9) | None => {
+                        //         buf.push(self.next());
+                        //     }
+                        //     _ => {
+                        //         break;
+                        //     }
+                        // }
+                        // buf.push(self.next());
                     }
-                    '+' | '-' => {
+                    Ok('+') | Ok('-') => {
                         break;
                     }
-                    x if x.is_digit(10) => {
-                        buf.push(x);
+                    Ok(cx) if cx.is_digit(10) => {
+                        // cursor.next();
+                        match cursor.next() {
+                            None => {
+                                // Finished
+                                break;
+                            }
+
+                            Some(Err(e)) => {
+                                return Err(Error::from(e));
+                            }
+
+                            Some(Ok(c)) => {
+                                buf.push(c);
+                            }
+                        }
+                        // buf.push(*cx);
                     }
-                    _ => break,
+                    Ok(_) => break,
                 }
             }
         }
 
-        // Check the NumericLiteral is not followed by an `IdentifierStart` or `DecimalDigit` character.
-        match cursor.peek() {
-            Some(r) => {
-                let c = r.map_err(|e| Error::IO(e))?;
-                if c.is_ascii_alphabetic() || c == '$' || c == '_' || c.is_ascii_digit() {
-                    return Err(Error::syntax("NumericLiteral token must not be followed by IdentifierStart nor DecimalDigit characters"));
-                }
-            },
-            _ => {}
-        }
+        // TODO
+        //self.check_after_numeric_literal()?;
 
         let num = match kind {
-            NumericKind::BigInt(base) => {
-                NumericLiteral::BigInt(
-                    BigInt::from_string_radix(&buf, base as u32).expect("Could not conver to BigInt")
+                NumericKind::BigInt(base) => {
+                    Numeric::BigInt(
+                        BigInt::from_string_radix(&buf, base as u32).expect("Could not convert to BigInt")
+                        )
+                }
+                NumericKind::Rational /* base: 10 */ => {
+                    Numeric::Rational(
+                        f64::from_str(&buf)
+                            .map_err(|_| Error::syntax("Could not convert value to f64"))?,
                     )
-            }
-            NumericKind::Rational /* base: 10 */ => {
-                NumericLiteral::Rational(
-                    f64::from_str(&buf)
-                        .map_err(|_| Error::syntax("Could not convert value to f64"))?,
-                )
-            }
-            NumericKind::Integer(base) => {
-                if let Ok(num) = i32::from_str_radix(&buf, base as u32) {
-                    NumericLiteral::Integer(
-                        num
-                    )
-                } else {
-                    let b = f64::from(base);
-                    let mut result = 0.0_f64;
-                    for c in buf.chars() {
-                        let digit = f64::from(c.to_digit(base as u32).unwrap());
-                        result = result * b + digit;
+                }
+                NumericKind::Integer(base) => {
+                    if let Ok(num) = i32::from_str_radix(&buf, base as u32) {
+                        Numeric::Integer(
+                            num
+                        )
+                    } else {
+                        let b = f64::from(base);
+                        let mut result = 0.0_f64;
+                        for c in buf.chars() {
+                            let digit = f64::from(c.to_digit(base as u32).unwrap());
+                            result = result * b + digit;
+                        }
+
+                        Numeric::Rational(result)
                     }
 
-                    NumericLiteral::Rational(result)
                 }
-
-            }
-        };
+            };
 
         Ok(Token::new(
             TokenKind::NumericLiteral(num),
@@ -362,5 +294,3 @@ impl<R> Tokenizer<R> for NumberLiteral {
         ))
     }
 }
-
-*/
