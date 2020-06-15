c66169e1bada3ce1c34db005023f847775a9e59f
Ryan Fickenscher <rsficken@gmail.com>
2020-06-15 00:45:55 -0700

    updated the test for the  property as suggested

--- boa/src/builtins/global_this/tests.rs
+++ boa/src/builtins/global_this/tests.rs
@@ -1,10 +1,10 @@
 use crate::exec;
 
 #[test]
-fn global_this_exists_on_global_object_and_evaluates_to_global_this_value() {
+fn global_this_exists_on_global_object_and_evaluates_to_an_object() {
     let scenario = r#"
-        globalThis;
+        typeof globalThis;
         "#;
 
-    assert_eq!(&exec(scenario), "globalThis");
+    assert_eq!(&exec(scenario), "object");
 }
