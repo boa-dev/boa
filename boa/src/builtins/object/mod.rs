a5014769a118fd7ba2ab5f495e727331956291c3
croraf <vrafaeli@msn.com>
2020-07-03 21:40:05 +0200

    Remove .into as suggested by clippy

--- boa/src/builtins/object/mod.rs
+++ boa/src/builtins/object/mod.rs
@@ -417,7 +417,7 @@ pub fn make_object(_: &Value, args: &[Value], ctx: &mut Interpreter) -> ResultVa
 pub fn create_builtin(_: &Value, args: &[Value], _: &mut Interpreter) -> ResultValue {
     let __proto__ = args.get(0).cloned().unwrap_or_else(Value::undefined);
     let new_object = Value::new_object_from_prototype(__proto__, ObjectData::Ordinary);
-    Ok(new_object.into())
+    Ok(new_object)
 }
 
 /// Uses the SameValue algorithm to check equality of objects
