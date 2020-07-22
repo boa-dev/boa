cce0626f1726ca3c58c747e77d9ca6efe7b70b46
KashParty <kashpartyofficial@gmail.com>
2020-07-22 22:05:21 +0100

    Added tests

--- boa/src/exec/tests.rs
+++ boa/src/exec/tests.rs
@@ -1163,3 +1163,22 @@ fn not_a_function() {
     "#;
     assert_eq!(forward(&mut engine, scenario), "TypeError: not a function");
 }
+
+#[test]
+fn comma_operator() {
+    let scenario = r#"
+        var a, b;
+        b = 10;
+        a = (b++, b);
+        a
+    "#;
+    assert_eq!(&exec(scenario), "11");
+
+    let scenario = r#"
+        var a, b;
+        b = 10;
+        a = (b += 5, b /= 3, b - 3);
+        a
+    "#;
+    assert_eq!(&exec(scenario), "2");
+}
