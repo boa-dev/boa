d781ed232cc9a578bac9270304bb7dff00dbc342
Paul Lancaster <paul@lancasterzone.com>
2020-08-02 14:17:25 +0100

    Added some cursor specific tests

--- boa/src/syntax/parser/cursor/tests.rs
+++ boa/src/syntax/parser/cursor/tests.rs
@@ -35,4 +35,195 @@ fn peek_skip_accending() {
             .kind(),
         TokenKind::identifier("d")
     );
+    assert_eq!(
+        *cur.peek_skip(2, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("c")
+    );
+    assert_eq!(
+        *cur.peek_skip(1, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("b")
+    );
+    assert_eq!(
+        *cur.peek(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("a")
+    );
+}
+
+#[test]
+fn peek_skip_next() {
+    let buf: &[u8] = "a b c d e f g h i".as_bytes();
+
+    let mut cur = Cursor::new(buf);
+
+    assert_eq!(
+        *cur.peek(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("a")
+    );
+    assert_eq!(
+        *cur.peek_skip(1, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("b")
+    );
+    assert_eq!(
+        *cur.peek_skip(2, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("c")
+    );
+    assert_eq!(
+        *cur.peek_skip(3, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("d")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("a")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("b")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("c")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("d")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("e")
+    );
+    assert_eq!(
+        *cur.peek(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("f")
+    );
+    assert_eq!(
+        *cur.peek_skip(1, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("g")
+    );
+    assert_eq!(
+        *cur.peek_skip(2, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("h")
+    );
+    assert_eq!(
+        *cur.peek_skip(3, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("i")
+    );
+}
+
+#[test]
+fn peek_skip_next_alternating() {
+    let buf: &[u8] = "a b c d e f g h i".as_bytes();
+
+    let mut cur = Cursor::new(buf);
+
+    assert_eq!(
+        *cur.peek(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("a")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("a")
+    );
+    assert_eq!(
+        *cur.peek_skip(1, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("c")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("b")
+    );
+    assert_eq!(
+        *cur.peek_skip(1, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("d")
+    );
+    assert_eq!(
+        *cur.peek_skip(3, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("f")
+    );
+    assert_eq!(
+        *cur.next(false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("c")
+    );
+    assert_eq!(
+        *cur.peek_skip(2, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("f")
+    );
+    assert_eq!(
+        *cur.peek_skip(3, false)
+            .unwrap()
+            .expect("Some value expected")
+            .kind(),
+        TokenKind::identifier("g")
+    );
 }
