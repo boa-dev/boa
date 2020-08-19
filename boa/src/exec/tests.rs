8451f0c1a243558f62b5b261e550969063fb34c9
Jonathan Dickinson <jonathanD@k2.com>
2020-08-19 10:59:21 -0700

    Redo rustfmt

--- boa/src/exec/tests.rs
+++ boa/src/exec/tests.rs
@@ -770,7 +770,9 @@ mod in_operator {
         let bar_obj = bar_val.as_object().unwrap();
         let foo_val = forward_val(&mut engine, "Foo").unwrap();
         let foo_obj = foo_val.as_object().unwrap();
-        assert!(bar_obj.prototype().strict_equals(&foo_obj.get_field("prototype").unwrap()));
+        assert!(bar_obj
+            .prototype()
+            .strict_equals(&foo_obj.get_field("prototype").unwrap()));
     }
 }
 
