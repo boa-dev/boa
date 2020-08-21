0261978b4b7cea741d6b76c94473db753a28ea05
Iban Eguia <razican@protonmail.ch>
2020-08-21 09:05:00 +0200

    Update boa/src/syntax/lexer/operator.rs

--- boa/src/syntax/lexer/operator.rs
+++ boa/src/syntax/lexer/operator.rs
@@ -14,7 +14,7 @@ use std::io::Read;
 macro_rules! vop {
     ($cursor:ident, $assign_op:expr, $op:expr) => ({
         match $cursor.peek()? {
-            None => Err(Error::syntax("Abrupt end: could not preview next value as part of operator")),
+            None => Err(Error::syntax("abrupt end - could not preview next value as part of the operator")),
             Some('=') => {
                 $cursor.next_char()?.expect("= token vanished");
                 $cursor.next_column();
