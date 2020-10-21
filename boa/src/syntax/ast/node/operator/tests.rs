8a9c7287e690db905811ba86c21ac9c8b5d202a7
Andrej Onufrák <andrej.onufrak@student.tuke.sk>
2020-10-21 19:33:12 +0200

    Implemented the `instanceof` operator

--- boa/src/syntax/ast/node/operator/tests.rs
+++ boa/src/syntax/ast/node/operator/tests.rs
@@ -26,3 +26,37 @@ fn assignmentoperator_rhs_throws_error() {
 
     assert_eq!(&exec(scenario), "\"ReferenceError: b is not defined\"");
 }
+
+#[test]
+fn instanceofoperator_rhs_not_object() {
+    let scenario = r#"
+        try {
+          let s = new String();
+          s instanceof 1
+        } catch (err) {
+          err.toString()
+        }
+        "#;
+
+    assert_eq!(
+        &exec(scenario),
+        "\"TypeError: right-hand side of 'instanceof' should be an object, got number\""
+    );
+}
+
+#[test]
+fn instanceofoperator_rhs_not_callable() {
+    let scenario = r#"
+        try {
+          let s = new String();
+          s instanceof {}
+        } catch (err) {
+          err.toString()
+        }
+        "#;
+
+    assert_eq!(
+        &exec(scenario),
+        "\"TypeError: right-hand side of 'instanceof' is not callable\""
+    );
+}
