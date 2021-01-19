99a6096fb11293f0275261249c1dd52fcceafb00
Jevan Chan <jevan.cnchan@gmail.com>
2021-01-19 12:52:38 -0800

    Update boa/src/syntax/lexer/string.rs
    Co-authored-by: tofpie <75836434+tofpie@users.noreply.github.com>

--- boa/src/syntax/lexer/string.rs
+++ boa/src/syntax/lexer/string.rs
@@ -212,7 +212,7 @@ impl StringLiteral {
             let mut code_point_buf = Vec::with_capacity(6);
             cursor.take_until(b'}', &mut code_point_buf)?;
 
-            // Safty: invalid UTF-8 bytes will be handled by returning Err in the following `u32::from_str_radix`
+            // Safety: invalid UTF-8 bytes will be handled by returning Err in the following `u32::from_str_radix`
             let code_point_str = unsafe { str::from_utf8_unchecked(code_point_buf.as_slice()) };
             // The `code_point_str` should represent a single unicode codepoint, convert to u32
             let code_point = u32::from_str_radix(&code_point_str, 16).map_err(|_| {
