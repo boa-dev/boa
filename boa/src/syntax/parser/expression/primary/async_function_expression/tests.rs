c6d8e6e39203f59c4eaa767fda6e2a7f9dc97962
0x7D2B <0x7D2B@protonmail.com>
2021-03-18 16:41:56 +0000

    Rustfmt

--- boa/src/syntax/parser/expression/primary/async_function_expression/tests.rs
+++ boa/src/syntax/parser/expression/primary/async_function_expression/tests.rs
@@ -42,29 +42,40 @@ fn check_nested_async_expression() {
             };
         };
         ",
-        vec![DeclarationList::Const(vec![Declaration::new(
-            "a",
-            Some(
-                AsyncFunctionExpr::new::<Option<Box<str>>, _, StatementList>(
-                    None,
-                    [],
-                    vec![DeclarationList::Const(vec![Declaration::new(
-                        "b",
-                        Some(
-                            AsyncFunctionExpr::new::<Option<Box<str>>, _, StatementList>(
-                                None,
-                                [],
-                                vec![Return::new::<_, _, Option<Box<str>>>(Const::from(1), None)
-                                    .into()]
-                                .into(),
-                            ).into(),
-                        ),
-                    )].into())
-                    .into()]
+        vec![DeclarationList::Const(
+            vec![Declaration::new(
+                "a",
+                Some(
+                    AsyncFunctionExpr::new::<Option<Box<str>>, _, StatementList>(
+                        None,
+                        [],
+                        vec![DeclarationList::Const(
+                            vec![Declaration::new(
+                                "b",
+                                Some(
+                                    AsyncFunctionExpr::new::<Option<Box<str>>, _, StatementList>(
+                                        None,
+                                        [],
+                                        vec![Return::new::<_, _, Option<Box<str>>>(
+                                            Const::from(1),
+                                            None,
+                                        )
+                                        .into()]
+                                        .into(),
+                                    )
+                                    .into(),
+                                ),
+                            )]
+                            .into(),
+                        )
+                        .into()]
+                        .into(),
+                    )
                     .into(),
-                ).into(),
-            ),
-        )].into())
+                ),
+            )]
+            .into(),
+        )
         .into()],
     );
 }
