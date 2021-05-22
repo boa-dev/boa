7287d1529985cbfdc5c1e7398d25c2b23e3b6c3c
Iban Eguia (Razican) <razican@protonmail.ch>
2021-05-22 19:27:03 +0200

    Fixed style with Prettier

--- boa_examples/scripts/calc.js
+++ boa_examples/scripts/calc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    add: function(a, b){
-        return a + b;
-    },
-    subtract: function(a, b){
-        return a - b;
-    },
-    multiply: function(a, b){
-        return a * b;
-    },
-    divide: function(a, b){
-        return a/b;
-    }
-}
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+};
