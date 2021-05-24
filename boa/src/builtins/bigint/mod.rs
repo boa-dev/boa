dec8b96cd1e413af282fc7e9a0fec27049df4d14
Iban Eguia (Razican) <razican@protonmail.ch>
2021-05-24 22:17:53 +0200

    New test

--- boa/src/builtins/bigint/mod.rs
+++ boa/src/builtins/bigint/mod.rs
@@ -42,7 +42,7 @@ mod tests;
 pub struct BigInt(num_bigint::BigInt);
 
 impl BuiltIn for BigInt {
-    const NAME: &'static str = "BigInt";
+    const NAME: &'static str = "BigInt2";
 
     fn attribute() -> Attribute {
         Attribute::WRITABLE | Attribute::NON_ENUMERABLE | Attribute::CONFIGURABLE
