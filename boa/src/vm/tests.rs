085a8080eab7c58ac3d9dcf06b71b1428f4df194
Jason Williams <jase.williams@gmail.com>
2021-06-04 18:37:28 +0100

    only have vm switched on

--- boa/src/vm/tests.rs
+++ boa/src/vm/tests.rs
@@ -1,7 +1,7 @@
 use crate::exec;
 
 #[test]
-fn vm_typeof_string() {
+fn typeof_string() {
     let typeof_object = r#"
         const a = "hello";
         typeof a;
@@ -10,10 +10,20 @@ fn vm_typeof_string() {
 }
 
 #[test]
-fn vm_typeof_number() {
+fn typeof_number() {
     let typeof_number = r#"
         let a = 1234;
         typeof a;
     "#;
     assert_eq!(&exec(typeof_number), "\"number\"");
+}
+
+#[test]
+fn basic_op() {
+    let basic_op = r#"
+        const a = 1;
+        const b = 2;
+        a + b
+    "#;
+    assert_eq!(&exec(basic_op), "3");
 }
