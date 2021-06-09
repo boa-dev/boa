b27052f19d8d2606cd1ac750cdf5139b88f5b6e7
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-09 14:42:34 -0700

    Added break formatting test

--- boa/src/syntax/ast/node/break_node/tests.rs
+++ boa/src/syntax/ast/node/break_node/tests.rs
@@ -1,5 +1,6 @@
 use crate::{
     exec::{Executable, InterpreterState},
+    parse,
     syntax::ast::node::Break,
     Context,
 };
@@ -16,4 +17,38 @@ fn check_post_state() {
         context.executor().get_current_state(),
         &InterpreterState::Break(Some("label".into()))
     );
+}
+
+#[test]
+fn fmt() {
+    // Blocks do not store their label, so we cannot test with
+    // the outer block having a label.
+    //
+    // TODO: Once block labels are implemented, this test should
+    // include them:
+    //
+    // ```
+    // outer: {
+    //     while (true) {
+    //         break outer;
+    //     }
+    //     skipped_call();
+    // }
+    // ```
+    let scenario = r#"
+        {
+            while (true) {
+                break outer;
+            }
+            skipped_call();
+        }
+        while (true) {
+            break;
+        }
+        "#[1..] // Remove the preceding newline
+        .lines()
+        .map(|l| &l[8..]) // Remove preceding whitespace from each line
+        .collect::<Vec<&'static str>>()
+        .join("\n");
+    assert_eq!(format!("{}", parse(&scenario, false).unwrap()), scenario);
 }
