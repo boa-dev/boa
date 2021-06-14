094c5b9faf9f850de09a3cdcbd80bad3896db234
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-14 14:01:01 -0700

    Added a formatting test for templates

--- boa/src/syntax/ast/node/template/tests.rs
+++ boa/src/syntax/ast/node/template/tests.rs
@@ -29,3 +29,20 @@ fn tagged_template() {
         r#"[ "result: ", " & ", "", "result: ", " \x26 ", "", 10, 20 ]"#
     );
 }
+
+#[test]
+fn fmt() {
+    super::super::test_formatting(
+        r#"
+        function tag(t, ...args) {
+            let a = [];
+            a = a.concat([t[0], t[1], t[2]]);
+            a = a.concat([t.raw[0], t.raw[1], t.raw[2]]);
+            a = a.concat([args[0], args[1]]);
+            return a;
+        };
+        let a = 10;
+        tag`result: ${a} \x26 ${a + 10}`;
+        "#,
+    );
+}
