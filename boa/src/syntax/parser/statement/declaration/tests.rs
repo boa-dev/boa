3dc3bdab0ae50489331d17b5ef61eac8112bde74
Neil Macneale <neil.macneale.v@gmail.com>
2021-07-09 13:09:04 -0700

    Improved redeclaration tests and comments

--- boa/src/syntax/parser/statement/declaration/tests.rs
+++ boa/src/syntax/parser/statement/declaration/tests.rs
@@ -215,14 +215,23 @@ fn multiple_const_declaration() {
 /// Checks for redeclaration errors.
 #[test]
 fn redeclaration_errors() {
-    // Throughout this test, we test four combinations:
+    // Most of these tests seem crazy at first. But they follow
+    // a few simple rules:
     //
-    // - let-var
-    // - var-let
-    // - let-let
-    // - var-var
+    // - Let statements stay within their block scope.
+    // - Var statements stay within their function scope.
+    // - The only time you can redeclare is when you use `var` and `var`.
+    // - We always check the innermost block first.
     //
-    // Depending on the block/function scope, these are sometimes valid, and sometimes not valid.
+    // This explains most of the tests. For example, `var a; { let a; }`
+    // seems like it should be invalid, but because we always check the
+    // innermost block first, it ends up evaluating to this: `{ let a; }; var a`.
+    // And now the solution is clear: the `let` goes out of scope before
+    // the var is declared.
+    //
+    // I think this entire system makes no sense, and I would never implement
+    // it this way if it were up to me. However, this is how all js parsers
+    // work, so this is how its implemented.
 
     // Same scope
     check_invalid("let a; var a;");
@@ -249,6 +258,10 @@ fn redeclaration_errors() {
     check_invalid("let a; { let a } { var a }");
     check_valid("var a; { var a } { var a }");
 
+    // Inside a function, everything is simple. The parameters act like `var`
+    // declarations, and they have the same scope as the outermost function
+    // block. Any surrounding variables do not exist when inside a function.
+
     // Function scoping
     check_invalid("function f(a) { let a; }");
     check_valid("function f(a) { var a; }");
@@ -258,9 +271,30 @@ fn redeclaration_errors() {
     check_valid("let a; function f(a) {}");
     check_valid("var a; function f(a) {}");
 
+    check_valid("let a; function f() { var a; }");
+    check_valid("var a; function f() { let a; }");
+    check_valid("let a; function f() { let a; }");
+    check_valid("var a; function f() { var a; }");
+
+    // Functions names are a bit more complex. They have the scope of `let`, but
+    // they can only be redeclared with themselves. So they will conflict with
+    // existing `let` and `var` statements. Once again, innermost blocks are
+    // evaluated first, so `function f() {}; { var f }` is invalid.
+
+    // Function definitions
     check_invalid("let f; function f() {}");
     check_invalid("var f; function f() {}");
     check_invalid("function f() {}; let f");
     check_invalid("function f() {}; var f");
     check_valid("function f() {}; function f() {}");
+
+    check_valid("{ function f() {} }; let f");
+    check_valid("{ function f() {} }; var f");
+    check_valid("let f; { function f() {} }");
+    check_valid("var f; { function f() {} }");
+
+    check_invalid("{ var f }; function f() {}");
+    check_valid("{ let f }; function f() {}");
+    check_invalid("function f() {}; { var f }");
+    check_valid("function f() {}; { let f }");
 }
