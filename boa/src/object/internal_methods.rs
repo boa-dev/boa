95d335c6566f7da0d358f5ca58a9cd7bef4f672c
raskad <32105367+raskad@users.noreply.github.com>
2021-08-10 03:16:03 +0200

    Change StringGetOwnProperty to use String::from_utf16_lossy

--- boa/src/object/internal_methods.rs
+++ boa/src/object/internal_methods.rs
@@ -620,10 +620,10 @@ impl GcObject {
                     return None;
                 }
 
-                let result_str = string.encode_utf16().nth(pos).map(|utf16_val| {
-                    char::from_u32(u32::from(utf16_val))
-                        .map_or_else(|| Value::from(format!("\\u{:x}", utf16_val)), Value::from)
-                })?;
+                let result_str = string
+                    .encode_utf16()
+                    .nth(pos)
+                    .map(|utf16_val| Value::from(String::from_utf16_lossy(&[utf16_val])))?;
 
                 let desc = PropertyDescriptor::builder()
                     .value(result_str)
