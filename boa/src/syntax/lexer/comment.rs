acbf3c9a2b4bfb8d77c1b0522d8ea22bd3639cf1
nekevss <nekevss@gmail.com>
2021-10-05 19:09:42 -0400

    removing comment linked to byte consumption

--- boa/src/syntax/lexer/comment.rs
+++ boa/src/syntax/lexer/comment.rs
@@ -112,7 +112,6 @@ impl<R> Tokenizer<R> for HashbangComment {
             if let Some(ch) = cursor.next_char()? {
                 if let Ok(c) = char::try_from(ch) {
                     if c == '\r' || c == '\n' || c == '\u{2028}' || c == '\u{2029}' {
-                        //still want to consume the byte to move to next line
                         break;
                     }
                 } else {
