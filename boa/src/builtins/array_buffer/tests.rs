88b2887d3354aa30c9c64daac4a4ab77077daa5c
nd419 <5161147+neeldug@users.noreply.github.com>
2021-10-28 18:08:54 +0100

    - adds more coverage

--- boa/src/builtins/array_buffer/tests.rs
+++ boa/src/builtins/array_buffer/tests.rs
@@ -10,3 +10,27 @@ fn constructor() {
     forward(&mut context, init);
     assert_eq!(forward(&mut context, "buffer.byteLength"), "8");
 }
+
+#[test]
+fn is_view() {
+    let mut context = Context::new();
+    let init = r#"
+        const buffer = new ArrayBuffer(16);
+        "#;
+    forward(&mut context, init);
+    assert_eq!(forward(&mut context, "ArrayBuffer.isView(new Int32Array())"), "true");
+}
+
+#[test]
+fn slice() {
+    let mut context = Context::new();
+    let init = r#"
+        const buffer = new ArrayBuffer(16);
+        const int32View = new Int32Array(buffer);
+
+        int32View[1] = 42;
+        const sliced = new Int32Array(buffer.slice(4, 12));
+        "#;
+    forward(&mut context, init);
+    assert_eq!(forward(&mut context, "sliced[0]"), "42");
+}
