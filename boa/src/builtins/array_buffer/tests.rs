026f8c90a2495c1a1003a64890c787698cd32b53
Iban Eguia Moraza <razican@protonmail.ch>
2021-12-04 11:15:15 +0100

    Added better tests

--- boa/src/builtins/array_buffer/tests.rs
+++ boa/src/builtins/array_buffer/tests.rs
@@ -1,7 +1,14 @@
 use super::*;
 
 #[test]
-fn ut_check_failed_allocation() {
+fn ut_sunnyy_day_create_byte_data_block() {
+    let mut context = Context::new();
+
+    assert!(create_byte_data_block(100, &mut context).is_ok())
+}
+
+#[test]
+fn ut_rainy_day_create_byte_data_block() {
     let mut context = Context::new();
 
     assert!(create_byte_data_block(usize::MAX, &mut context).is_err())
