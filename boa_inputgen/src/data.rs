750d166984388e09c2cfaf41b3d3a65f9b1351af
Addison Crump <me@addisoncrump.info>
2022-03-17 12:39:28 -0500

    docs

--- boa_inputgen/src/data.rs
+++ boa_inputgen/src/data.rs
@@ -1,3 +1,5 @@
+//! Fuzz data generation.
+
 use std::collections::HashSet;
 
 use arbitrary::{size_hint, Arbitrary, Unstructured};
@@ -23,6 +25,7 @@ static ALPHANUM: Lazy<Vec<u8>> = Lazy::new(|| {
     all
 });
 
+/// A valid name for use as an identifier.
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Name {
     name: String,
@@ -30,25 +33,30 @@ struct Name {
 
 impl<'a> arbitrary::Arbitrary<'a> for Name {
     fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
-        let first = u8::arbitrary(u)?; // at least one
+        // generate a valid identifier; starts with at least one alphabetic character
+        let first = u8::arbitrary(u)?;
         let first = ALPHA[(first as usize) % ALPHA.len()];
+
+        // remaining characters are alphanumeric
         let mut chars: Vec<u8> = vec![first];
         let mut second: Vec<u8> = Arbitrary::arbitrary(u)?;
         second
             .iter_mut()
             .for_each(|c| *c = ALPHANUM[(*c as usize) % ALPHANUM.len()]);
         chars.extend(second);
+
         Ok(Self {
-            name: String::from_utf8(chars).unwrap(),
+            name: String::from_utf8(chars).expect("Only valid characters used."),
         })
     }
 
+    // size is at least one u8 and a vec of u8s for the rest
     fn size_hint(depth: usize) -> (usize, Option<usize>) {
         size_hint::and(u8::size_hint(depth), Vec::<u8>::size_hint(depth))
     }
 }
 
-/// Fuzz data which can be arbitrarily generated and used to test boa's parser, compiler, and vm
+/// Fuzz data which can be arbitrarily generated and used to test boa's parser, compiler, and vm.
 #[derive(Debug, Clone)]
 pub struct FuzzData {
     source: String,
@@ -56,15 +64,23 @@ pub struct FuzzData {
 
 impl<'a> Arbitrary<'a> for FuzzData {
     fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
-        let first_name = Name::arbitrary(u)?; // need at least one
+        // we need at least one name or we'll mod by zero when trying to get a name
+        let first_name = Name::arbitrary(u)?;
+        // generate the rest
         let mut vars = HashSet::<Name>::arbitrary(u)?;
         vars.insert(first_name);
+
+        // generate a javascript sample
         let mut sample = StatementList::arbitrary(u)?;
+
+        // notify the interner of the symbols we're using
         let mut interner = Interner::with_capacity(vars.len());
         let syms = vars
             .into_iter()
             .map(|var| interner.get_or_intern(var.name))
             .collect::<Vec<_>>();
+
+        // walk the AST and ensure that all identifiers are valid
         replace_syms(&syms, &mut sample);
         Ok(Self {
             source: sample.to_interned_string(&interner),
