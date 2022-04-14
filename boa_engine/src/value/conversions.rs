17aa57040d161aab174c8b344d60b174bdf362c6
Halid Odat <halidodat@gmail.com>
2022-04-14 23:17:52 +0000

    Feature `JsTypedArray`s (#2003)
    This PR adds a wrapper around the raw builtins for JavaScript typed arrays, like #1746

--- boa_engine/src/value/conversions.rs
+++ boa_engine/src/value/conversions.rs
@@ -42,6 +42,18 @@ impl Display for TryFromCharError {
     }
 }
 
+impl From<f32> for JsValue {
+    #[allow(clippy::float_cmp)]
+    #[inline]
+    fn from(value: f32) -> Self {
+        // if value as i32 as f64 == value {
+        //     Self::Integer(value as i32)
+        // } else {
+        Self::Rational(value.into())
+        // }
+    }
+}
+
 impl From<f64> for JsValue {
     #[allow(clippy::float_cmp)]
     #[inline]
@@ -54,15 +66,31 @@ impl From<f64> for JsValue {
     }
 }
 
-impl From<f32> for JsValue {
-    #[allow(clippy::float_cmp)]
+impl From<u8> for JsValue {
     #[inline]
-    fn from(value: f32) -> Self {
-        // if value as i32 as f32 == value {
-        //     Self::Integer(value as i32)
-        // } else {
-        Self::Rational(value.into())
-        // }
+    fn from(value: u8) -> Self {
+        Self::Integer(value.into())
+    }
+}
+
+impl From<i8> for JsValue {
+    #[inline]
+    fn from(value: i8) -> Self {
+        Self::Integer(value.into())
+    }
+}
+
+impl From<u16> for JsValue {
+    #[inline]
+    fn from(value: u16) -> Self {
+        Self::Integer(value.into())
+    }
+}
+
+impl From<i16> for JsValue {
+    #[inline]
+    fn from(value: i16) -> Self {
+        Self::Integer(value.into())
     }
 }
 
