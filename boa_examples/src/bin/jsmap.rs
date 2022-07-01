4d2f66dc9fab59cfe1dbc378e57328a12df77f71
nekevss <nekevss@gmail.com>
2022-07-01 16:57:41 -0400

    Remove overused

--- boa_examples/src/bin/jsmap.rs
+++ boa_examples/src/bin/jsmap.rs
@@ -11,24 +11,23 @@ fn main() -> JsResult<()> {
     let map = JsMap::new(context);
 
     // Set a key-value for the map.
-    map.set(JsValue::new("Key-1"), JsValue::new("Value-1"), context)?;
+    map.set("Key-1", "Value-1", context)?;
 
-    let map_check = map.has(JsValue::new("Key-1"), context)?;
-    assert_eq!(map_check, JsValue::new(true)); // true
+    let map_check = map.has("Key-1", context)?;
+    assert_eq!(map_check, true.into()); // true
 
     // Set a second key-value to the same map.
-    map.set(JsValue::new("Key-2"), JsValue::new("Value-2"), context)?;
+    map.set(2, 4, context)?;
 
-    assert_eq!(map.get_size(context)?, JsValue::new(2)); //true
+    assert_eq!(map.get_size(context)?, 2.into()); //true
 
-    let current_key_one = map.get(JsValue::new("Key-1"), context)?;
-    assert_eq!(current_key_one, JsValue::new("Value-1"));
-
+    assert_eq!(map.get("Key-1", context)?, "Value-1".into());
+    assert_eq!(map.get(2, context)?, 4.into());
     // Delete an entry with a provided key.
-    map.delete(JsValue::new("Key-1"), context)?;
-    assert_eq!(map.get_size(context)?, JsValue::new(1));
+    map.delete("Key-1", context)?;
+    assert_eq!(map.get_size(context)?, 1.into());
 
-    let deleted_key_one = map.get(JsValue::new("Key-1"), context)?;
+    let deleted_key_one = map.get("Key-1", context)?;
 
     assert_eq!(deleted_key_one, JsValue::undefined());
 
@@ -49,16 +48,9 @@ fn main() -> JsResult<()> {
     // Create a map from the JsArray using it's iterable property.
     let iter_map = JsMap::from_js_iterable(&js_array.into(), context)?;
 
-    assert_eq!(
-        iter_map.get(JsValue::new("first-key"), context)?,
-        "first-value".into()
-    );
+    assert_eq!(iter_map.get("first-key", context)?, "first-value".into());
 
-    iter_map.set(
-        JsValue::new("third-key"),
-        JsValue::new("third-value"),
-        context,
-    )?;
+    iter_map.set("third-key", "third-value", context)?;
 
     assert_eq!(iter_map.get_size(context)?, JsValue::new(3));
 
