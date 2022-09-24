d268c6c2d7630473426d57b472c8e08b284b8118
nekevss <nekevss@gmail.com>
2022-09-23 20:49:42 -0400

    Add methods to dataview methods

--- boa_engine/src/object/jsdataview.rs
+++ boa_engine/src/object/jsdataview.rs
@@ -15,8 +15,6 @@ pub struct JsDataview {
 
 
 impl JsDataview {
-
-
     #[inline]
     pub fn from_object(object: JsObject, context: &mut Context) -> Self {
         if object.borrow().is_dataview() {
@@ -24,6 +22,121 @@ impl JsDataview {
         } else {
             context.throw_type_error("object is not a Dataview")
         }
+    }
+
+    #[inline]
+    pub fn buffer(&self, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_buffer(&self.inner.clone().into(), &[], context)
+    }
+
+    #[inline]
+    pub fn byte_length(&self, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_byte_length(&self.inner.clone().into(), &[], context)
+    }
+
+    #[inline]
+    pub fn byte_offset(&self, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_byte_offset(&self.inner.clone().into(), &[], context)
+    }
+
+    #[inline]
+    pub fn get_big_int64(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_big_int64(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_big_uint64(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_big_uint64(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_float32(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_float32(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_float64(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_float64(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_int8(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_int8(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_int16(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_int16(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_int32(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_int32(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+    
+    #[inline]
+    pub fn get_uint8(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_uint8(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_unit16(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_uint16(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn get_unit32(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::get_uint32(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_big_int64(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_big_int64(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_big_uint64(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_big_uint64(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_float32(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_float32(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_float64(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_float64(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_int8(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_int8(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_int16(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_int16(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_int32(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_int32(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+    
+    #[inline]
+    pub fn set_uint8(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_uint8(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_unit16(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_uint16(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
+    }
+
+    #[inline]
+    pub fn set_unit32(&self, byte_offset: usize, is_little_edian: bool, context: &mut Context) -> JsResult<JsValue> {
+        DataView::set_uint32(&self.inner.clone().into(), &[byte_offset.into(), is_little_edian.into()], context)
     }
 }
 
