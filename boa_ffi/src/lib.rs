99d1a8978efe895d314f3885a7f8169603f5ce71
Blake Ramsdell <blaker@gmail.com>
2022-09-26 14:43:41 -0700

    Make boa_exec return a const char*

--- boa_ffi/src/lib.rs
+++ boa_ffi/src/lib.rs
@@ -1,12 +1,17 @@
 use boa_engine::Context;
-use core::ffi::{c_char, CStr};
+use std::ffi::{c_char, CStr, CString};
 
 #[no_mangle]
-pub extern "C" fn boa_exec(src_bytes: *const c_char){
+pub extern "C" fn boa_exec(src_bytes: *const c_char) -> *const c_char {
     let c_str: &CStr = unsafe { CStr::from_ptr(src_bytes) };
 
-    let _return_value = match Context::default().eval(c_str.to_bytes()) {
+    let return_value = match Context::default().eval(c_str.to_bytes()) {
         Ok(value) => value.display().to_string(),
         Err(error) => error.display().to_string(),
-    }; // .as_bytes().as_mut_ptr();
+    };
+
+    let s = CString::new(return_value).unwrap();
+    let p = s.as_ptr();
+    std::mem::forget(s);
+    p
 }
--- boa_ffi/test/boa_test.c
+++ boa_ffi/test/boa_test.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
+
 #include "boa.h"
 
 int main(void)
 {
-    boa_exec("console.log('hello from C from Rust from JavaScript!');");
+    const char* return_value = NULL;
+    return_value = boa_exec("console.log('hello from C from Rust from JavaScript!');");
+
+    printf("Got back: \"%s\"\n", return_value);
     return 0;
 }
