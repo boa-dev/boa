a60e3f22759c62d7453bdff473531b24fe104f98
Blake Ramsdell <blaker@gmail.com>
2022-09-30 16:39:03 -0700

    Add function comments

--- boa_ffi/src/lib.rs
+++ boa_ffi/src/lib.rs
@@ -3,6 +3,19 @@
 use boa_engine::Context;
 use std::ffi::{c_char, CStr, CString};
 
+/// Evaluates the given code by compiling down to bytecode, interpreting the
+/// bytecode into a value, then converting the value to a UTF-8 encoded C
+/// string using the default context
+///
+/// The returned pointer should be freed with `boa_free_string`
+///
+/// # Arguments
+///
+/// * `src_bytes` - A zero-terminated UTF-8 encoded string
+///
+/// # Safety
+///
+/// The `src_bytes` argument better not be a bad pointer or bad things will happen.
 #[no_mangle]
 pub unsafe extern "C" fn boa_exec(src_bytes: *const c_char) -> *mut c_char {
     let c_str: &CStr = unsafe { CStr::from_ptr(src_bytes) };
@@ -16,6 +29,15 @@ pub unsafe extern "C" fn boa_exec(src_bytes: *const c_char) -> *mut c_char {
     s.into_raw()
 }
 
+/// Free the string result from a previous call to `boa_exec`
+///
+/// # Arguments
+///
+/// * `src_bytes` - The string pointer from a previous call to `boa_exec`
+///
+/// # Safety
+///
+/// The `src_bytes` argument better not be a bad pointer or bad things will happen.
 #[no_mangle]
 pub unsafe extern "C" fn boa_free_string(src_bytes: *mut c_char) {
     unsafe {
