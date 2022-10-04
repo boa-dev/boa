72caeb1577a9a17b38aedd976d5d4e1adb9a322a
Blake Ramsdell <blaker@gmail.com>
2022-10-04 00:00:01 -0700

    More generic boa_test to support testing

--- boa_ffi/test/boa_test.c
+++ boa_ffi/test/boa_test.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "boa.h"
 
 int main(void)
 {
     char* return_value = NULL;
-    return_value = boa_exec("console.log('hello from C from Rust from JavaScript!');");
+    char buffer[4096] = {0};
+    size_t bytes_read = 0;
 
-    printf("Got back: \"%s\"\n", return_value);
+    // Read up to sizeof(buffer) from stdin
+    bytes_read = fread(buffer, 1, sizeof(buffer), stdin);
 
+    // Bail on error
+    if (ferror(stdin))
+    {
+        perror("boa_test");
+        return 1;
+    }
+
+    // If it's more than our buffer size, sorry
+    if (bytes_read >= sizeof(buffer))
+    {
+        return 2;
+    }
+
+    // Get a zero-terminated result
+    return_value = boa_exec(buffer);
+
+    // Put it on stdout
+    fwrite(return_value, 1, strlen(return_value), stdout);
+
+    // Free the result
     boa_free_string(return_value);
-    printf("All freed!\n");
 
     return 0;
 }
