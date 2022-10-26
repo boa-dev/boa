fc1d3bd5f1fbc5ce88ff72697745afe8dda5b269
Blake Ramsdell <blaker@gmail.com>
2022-10-26 15:29:50 -0700

    Simplify get_output string creation

--- boa_ffi/src/tests.rs
+++ boa_ffi/src/tests.rs
@@ -18,7 +18,7 @@ fn get_output(input: &str) -> String {
     
     let output = child.wait_with_output().unwrap();
 
-    String::from(str::from_utf8(&output.stdout).unwrap())
+    String::from_utf8(output.stdout).unwrap()
 }
 
 #[test]
