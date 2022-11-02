a4887515f698ab3cd8414bc7c0f6e1047ba06d32
Addison Crump <addison.crump@cispa.de>
2022-11-02 18:46:13 +0100

    idempotency check improvement

--- boa_engine/fuzz/fuzz_targets/parser-idempotency.rs
+++ boa_engine/fuzz/fuzz_targets/parser-idempotency.rs
@@ -10,28 +10,44 @@ use std::error::Error;
 use std::io::Cursor;
 
 fn do_fuzz(mut data: FuzzData) -> Result<(), Box<dyn Error>> {
-    let interned = data.ast.to_interned_string(data.context.interner());
+    let original = data.ast.to_interned_string(data.context.interner());
 
-    let mut parser = Parser::new(Cursor::new(&interned));
+    let mut parser = Parser::new(Cursor::new(&original));
 
     let before = data.context.interner().len();
     // For a variety of reasons, we may not actually produce valid code here (e.g., nameless function).
     // Fail fast and only make the next checks if we were valid.
-    if let Ok(other) = parser.parse_all(&mut data.context) {
-        let after = data.context.interner().len();
+    if let Ok(first) = parser.parse_all(&mut data.context) {
+        let after_first = data.context.interner().len();
+        let first_interned = first.to_interned_string(data.context.interner());
 
         assert_eq!(
             before,
-            after,
+            after_first,
             "The number of interned symbols changed; a new string was read.\nBefore:\n{}\nAfter:\n{}",
-            interned,
-            other.to_interned_string(data.context.interner())
+            original,
+            first_interned
+        );
+        let mut parser = Parser::new(Cursor::new(&first_interned));
+
+        // Now, we most assuredly should produce valid code. It has already gone through a first pass.
+        let second = parser
+            .parse_all(&mut data.context)
+            .expect("Could not parse the first-pass interned copy.");
+        let after_second = data.context.interner().len();
+        assert_eq!(
+            after_first,
+            after_second,
+            "The number of interned symbols changed; a new string was read.\nBefore:\n{}\nAfter:\n{}",
+            first_interned,
+            second.to_interned_string(data.context.interner())
         );
         assert_eq!(
-            interned,
-            other.to_interned_string(data.context.interner()),
-            "ASTs before and after did not match; AST after:\n{:#?}",
-            other
+            first,
+            second,
+            "Expected the same AST after two intern passes, but found dissimilar.\nFirst:\n{}\nSecond:\n{}",
+            first_interned,
+            second.to_interned_string(data.context.interner())
         );
     }
     Ok(())
