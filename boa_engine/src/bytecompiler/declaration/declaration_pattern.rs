5692e2369e6be80f959e048bd027d4e1d48006b9
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2022-11-15 14:34:00 -0800

    add declaration, add more statements

---
+++ boa_engine/src/bytecompiler/declaration/declaration_pattern.rs
@@ -0,0 +1,263 @@
+use boa_ast::{
+    pattern::{ArrayPatternElement, ObjectPatternElement, Pattern},
+    property::PropertyName,
+};
+
+use crate::{
+    bytecompiler::{Access, ByteCompiler, Literal},
+    vm::{BindingOpcode, Opcode},
+    JsResult,
+};
+
+pub(crate) fn compile_declaration_pattern_impl<'b>(
+    byte_compiler: &mut ByteCompiler<'b>,
+    pattern: &Pattern,
+    def: BindingOpcode,
+) -> JsResult<()> {
+    match pattern {
+        Pattern::Object(pattern) => {
+            byte_compiler.emit_opcode(Opcode::ValueNotNullOrUndefined);
+
+            byte_compiler.emit_opcode(Opcode::RequireObjectCoercible);
+
+            let mut additional_excluded_keys_count = 0;
+            let rest_exits = pattern.has_rest();
+
+            for binding in pattern.bindings() {
+                use ObjectPatternElement::{
+                    AssignmentPropertyAccess, AssignmentRestPropertyAccess, Pattern, RestProperty,
+                    SingleName,
+                };
+
+                match binding {
+                    //  SingleNameBinding : BindingIdentifier Initializer[opt]
+                    SingleName {
+                        ident,
+                        name,
+                        default_init,
+                    } => {
+                        byte_compiler.emit_opcode(Opcode::Dup);
+                        match name {
+                            PropertyName::Literal(name) => {
+                                let index = byte_compiler.get_or_insert_name((*name).into());
+                                byte_compiler.emit(Opcode::GetPropertyByName, &[index]);
+                            }
+                            PropertyName::Computed(node) => {
+                                byte_compiler.compile_expr(node, true)?;
+                                if rest_exits {
+                                    byte_compiler.emit_opcode(Opcode::GetPropertyByValuePush);
+                                } else {
+                                    byte_compiler.emit_opcode(Opcode::GetPropertyByValue);
+                                }
+                            }
+                        }
+
+                        if let Some(init) = default_init {
+                            let skip =
+                                byte_compiler.emit_opcode_with_operand(Opcode::JumpIfNotUndefined);
+                            byte_compiler.compile_expr(init, true)?;
+                            byte_compiler.patch_jump(skip);
+                        }
+                        byte_compiler.emit_binding(def, *ident);
+
+                        if rest_exits && name.computed().is_some() {
+                            byte_compiler.emit_opcode(Opcode::Swap);
+                            additional_excluded_keys_count += 1;
+                        }
+                    }
+                    //  BindingRestProperty : ... BindingIdentifier
+                    RestProperty {
+                        ident,
+                        excluded_keys,
+                    } => {
+                        byte_compiler.emit_opcode(Opcode::PushEmptyObject);
+
+                        for key in excluded_keys {
+                            byte_compiler.emit_push_literal(Literal::String(
+                                byte_compiler
+                                    .interner()
+                                    .resolve_expect(key.sym())
+                                    .into_common(false),
+                            ));
+                        }
+
+                        byte_compiler.emit(
+                            Opcode::CopyDataProperties,
+                            &[excluded_keys.len() as u32, additional_excluded_keys_count],
+                        );
+                        byte_compiler.emit_binding(def, *ident);
+                    }
+                    AssignmentRestPropertyAccess {
+                        access,
+                        excluded_keys,
+                    } => {
+                        byte_compiler.emit_opcode(Opcode::Dup);
+                        byte_compiler.emit_opcode(Opcode::PushEmptyObject);
+                        for key in excluded_keys {
+                            byte_compiler.emit_push_literal(Literal::String(
+                                byte_compiler
+                                    .interner()
+                                    .resolve_expect(key.sym())
+                                    .into_common(false),
+                            ));
+                        }
+                        byte_compiler
+                            .emit(Opcode::CopyDataProperties, &[excluded_keys.len() as u32, 0]);
+                        byte_compiler.access_set(
+                            Access::Property { access },
+                            false,
+                            ByteCompiler::access_set_top_of_stack_expr_fn,
+                        )?;
+                    }
+                    AssignmentPropertyAccess {
+                        name,
+                        access,
+                        default_init,
+                    } => {
+                        byte_compiler.emit_opcode(Opcode::Dup);
+                        match name {
+                            PropertyName::Literal(name) => {
+                                let index = byte_compiler.get_or_insert_name((*name).into());
+                                byte_compiler.emit(Opcode::GetPropertyByName, &[index]);
+                            }
+                            PropertyName::Computed(node) => {
+                                byte_compiler.compile_expr(node, true)?;
+                                if rest_exits {
+                                    byte_compiler.emit_opcode(Opcode::GetPropertyByValuePush);
+                                } else {
+                                    byte_compiler.emit_opcode(Opcode::GetPropertyByValue);
+                                }
+                            }
+                        }
+
+                        if let Some(init) = default_init {
+                            let skip =
+                                byte_compiler.emit_opcode_with_operand(Opcode::JumpIfNotUndefined);
+                            byte_compiler.compile_expr(init, true)?;
+                            byte_compiler.patch_jump(skip);
+                        }
+
+                        byte_compiler.access_set(
+                            Access::Property { access },
+                            false,
+                            ByteCompiler::access_set_top_of_stack_expr_fn,
+                        )?;
+
+                        if rest_exits && name.computed().is_some() {
+                            byte_compiler.emit_opcode(Opcode::Swap);
+                            additional_excluded_keys_count += 1;
+                        }
+                    }
+                    Pattern {
+                        name,
+                        pattern,
+                        default_init,
+                    } => {
+                        byte_compiler.emit_opcode(Opcode::Dup);
+                        match name {
+                            PropertyName::Literal(name) => {
+                                let index = byte_compiler.get_or_insert_name((*name).into());
+                                byte_compiler.emit(Opcode::GetPropertyByName, &[index]);
+                            }
+                            PropertyName::Computed(node) => {
+                                byte_compiler.compile_expr(node, true)?;
+                                byte_compiler.emit_opcode(Opcode::GetPropertyByValue);
+                            }
+                        }
+
+                        if let Some(init) = default_init {
+                            let skip =
+                                byte_compiler.emit_opcode_with_operand(Opcode::JumpIfNotUndefined);
+                            byte_compiler.compile_expr(init, true)?;
+                            byte_compiler.patch_jump(skip);
+                        }
+
+                        byte_compiler.compile_declaration_pattern(pattern, def)?;
+                    }
+                }
+            }
+
+            if !rest_exits {
+                byte_compiler.emit_opcode(Opcode::Pop);
+            }
+        }
+        Pattern::Array(pattern) => {
+            byte_compiler.emit_opcode(Opcode::ValueNotNullOrUndefined);
+            byte_compiler.emit_opcode(Opcode::InitIterator);
+
+            for binding in pattern.bindings().iter() {
+                use ArrayPatternElement::{
+                    Elision, Pattern, PatternRest, PropertyAccess, PropertyAccessRest, SingleName,
+                    SingleNameRest,
+                };
+
+                match binding {
+                    // ArrayBindingPattern : [ Elision ]
+                    Elision => {
+                        byte_compiler.emit_opcode(Opcode::IteratorNext);
+                        byte_compiler.emit_opcode(Opcode::Pop);
+                    }
+                    // SingleNameBinding : BindingIdentifier Initializer[opt]
+                    SingleName {
+                        ident,
+                        default_init,
+                    } => {
+                        byte_compiler.emit_opcode(Opcode::IteratorNext);
+                        if let Some(init) = default_init {
+                            let skip =
+                                byte_compiler.emit_opcode_with_operand(Opcode::JumpIfNotUndefined);
+                            byte_compiler.compile_expr(init, true)?;
+                            byte_compiler.patch_jump(skip);
+                        }
+                        byte_compiler.emit_binding(def, *ident);
+                    }
+                    PropertyAccess { access } => {
+                        byte_compiler.emit_opcode(Opcode::IteratorNext);
+                        byte_compiler.access_set(
+                            Access::Property { access },
+                            false,
+                            ByteCompiler::access_set_top_of_stack_expr_fn,
+                        )?;
+                    }
+                    // BindingElement : BindingPattern Initializer[opt]
+                    Pattern {
+                        pattern,
+                        default_init,
+                    } => {
+                        byte_compiler.emit_opcode(Opcode::IteratorNext);
+
+                        if let Some(init) = default_init {
+                            let skip =
+                                byte_compiler.emit_opcode_with_operand(Opcode::JumpIfNotUndefined);
+                            byte_compiler.compile_expr(init, true)?;
+                            byte_compiler.patch_jump(skip);
+                        }
+
+                        byte_compiler.compile_declaration_pattern(pattern, def)?;
+                    }
+                    // BindingRestElement : ... BindingIdentifier
+                    SingleNameRest { ident } => {
+                        byte_compiler.emit_opcode(Opcode::IteratorToArray);
+                        byte_compiler.emit_binding(def, *ident);
+                    }
+                    PropertyAccessRest { access } => {
+                        byte_compiler.emit_opcode(Opcode::IteratorToArray);
+                        byte_compiler.access_set(
+                            Access::Property { access },
+                            false,
+                            ByteCompiler::access_set_top_of_stack_expr_fn,
+                        )?;
+                    }
+                    // BindingRestElement : ... BindingPattern
+                    PatternRest { pattern } => {
+                        byte_compiler.emit_opcode(Opcode::IteratorToArray);
+                        byte_compiler.compile_declaration_pattern(pattern, def)?;
+                    }
+                }
+            }
+
+            byte_compiler.emit_opcode(Opcode::IteratorClose);
+        }
+    }
+    Ok(())
+}
---
+++ boa_engine/src/bytecompiler/statement/try.rs
@@ -0,0 +1,130 @@
+use boa_ast::{declaration::Binding, operations::bound_names, statement::Try};
+
+use crate::{
+    bytecompiler::{ByteCompiler, Label},
+    vm::{BindingOpcode, Opcode},
+    JsResult,
+};
+
+pub(crate) fn compile_try<'b>(
+    byte_compiler: &mut ByteCompiler<'b>,
+    t: &Try,
+    use_expr: bool,
+    configurable_globals: bool,
+) -> JsResult<()> {
+    byte_compiler.push_try_control_info(t.finally().is_some());
+    let try_start = byte_compiler.next_opcode_location();
+    byte_compiler.emit(Opcode::TryStart, &[ByteCompiler::DUMMY_ADDRESS, 0]);
+    byte_compiler.context.push_compile_time_environment(false);
+    let push_env = byte_compiler.emit_opcode_with_two_operands(Opcode::PushDeclarativeEnvironment);
+
+    byte_compiler.create_decls(t.block().statement_list(), configurable_globals);
+    byte_compiler.compile_statement_list(
+        t.block().statement_list(),
+        use_expr,
+        configurable_globals,
+    )?;
+
+    let (num_bindings, compile_environment) = byte_compiler.context.pop_compile_time_environment();
+    let index_compile_environment = byte_compiler.push_compile_environment(compile_environment);
+    byte_compiler.patch_jump_with_target(push_env.0, num_bindings as u32);
+    byte_compiler.patch_jump_with_target(push_env.1, index_compile_environment as u32);
+    byte_compiler.emit_opcode(Opcode::PopEnvironment);
+    byte_compiler.emit_opcode(Opcode::TryEnd);
+
+    let finally = byte_compiler.jump();
+    byte_compiler.patch_jump(Label { index: try_start });
+
+    if let Some(catch) = t.catch() {
+        byte_compiler.push_try_control_info_catch_start();
+        let catch_start = if t.finally().is_some() {
+            Some(byte_compiler.emit_opcode_with_operand(Opcode::CatchStart))
+        } else {
+            None
+        };
+        byte_compiler.context.push_compile_time_environment(false);
+        let push_env =
+            byte_compiler.emit_opcode_with_two_operands(Opcode::PushDeclarativeEnvironment);
+        if let Some(binding) = catch.parameter() {
+            match binding {
+                Binding::Identifier(ident) => {
+                    byte_compiler
+                        .context
+                        .create_mutable_binding(*ident, false, false);
+                    byte_compiler.emit_binding(BindingOpcode::InitLet, *ident);
+                }
+                Binding::Pattern(pattern) => {
+                    for ident in bound_names(pattern) {
+                        byte_compiler
+                            .context
+                            .create_mutable_binding(ident, false, false);
+                    }
+                    byte_compiler.compile_declaration_pattern(pattern, BindingOpcode::InitLet)?;
+                }
+            }
+        } else {
+            byte_compiler.emit_opcode(Opcode::Pop);
+        }
+
+        byte_compiler.create_decls(catch.block().statement_list(), configurable_globals);
+        byte_compiler.compile_statement_list(
+            catch.block().statement_list(),
+            use_expr,
+            configurable_globals,
+        )?;
+
+        let (num_bindings, compile_environment) =
+            byte_compiler.context.pop_compile_time_environment();
+        let index_compile_environment = byte_compiler.push_compile_environment(compile_environment);
+        byte_compiler.patch_jump_with_target(push_env.0, num_bindings as u32);
+        byte_compiler.patch_jump_with_target(push_env.1, index_compile_environment as u32);
+        byte_compiler.emit_opcode(Opcode::PopEnvironment);
+        if let Some(catch_start) = catch_start {
+            byte_compiler.emit_opcode(Opcode::CatchEnd);
+            byte_compiler.patch_jump(catch_start);
+        } else {
+            byte_compiler.emit_opcode(Opcode::CatchEnd2);
+        }
+    }
+
+    byte_compiler.patch_jump(finally);
+
+    if let Some(finally) = t.finally() {
+        byte_compiler.emit_opcode(Opcode::FinallyStart);
+        let finally_start_address = byte_compiler.next_opcode_location();
+        byte_compiler.push_try_control_info_finally_start(Label {
+            index: finally_start_address,
+        });
+        byte_compiler.patch_jump_with_target(
+            Label {
+                index: try_start + 4,
+            },
+            finally_start_address,
+        );
+
+        byte_compiler.context.push_compile_time_environment(false);
+        let push_env =
+            byte_compiler.emit_opcode_with_two_operands(Opcode::PushDeclarativeEnvironment);
+
+        byte_compiler.create_decls(finally.block().statement_list(), configurable_globals);
+        byte_compiler.compile_statement_list(
+            finally.block().statement_list(),
+            false,
+            configurable_globals,
+        )?;
+
+        let (num_bindings, compile_environment) =
+            byte_compiler.context.pop_compile_time_environment();
+        let index_compile_environment = byte_compiler.push_compile_environment(compile_environment);
+        byte_compiler.patch_jump_with_target(push_env.0, num_bindings as u32);
+        byte_compiler.patch_jump_with_target(push_env.1, index_compile_environment as u32);
+        byte_compiler.emit_opcode(Opcode::PopEnvironment);
+
+        byte_compiler.emit_opcode(Opcode::FinallyEnd);
+        byte_compiler.pop_try_control_info(Some(finally_start_address));
+    } else {
+        byte_compiler.pop_try_control_info(None);
+    }
+
+    Ok(())
+}
