014d343dee577a0ccd29f17e4bc73af2046ce326
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2022-11-15 14:34:00 -0800

    add declaration, add more statements

---
+++ boa_engine/src/bytecompiler/declaration/mod.rs
@@ -0,0 +1,3 @@
+mod declaration_pattern;
+
+pub(crate) use declaration_pattern::compile_declaration_pattern_impl;
--- boa_engine/src/bytecompiler/statement/mod.rs
+++ boa_engine/src/bytecompiler/statement/mod.rs
@@ -1,16 +1,17 @@
 use boa_ast::{
-    statement::{If, Labelled, LabelledItem},
+    statement::{Break, If, Labelled, LabelledItem, Switch},
     Statement,
 };
 
-use crate::JsResult;
+use crate::{vm::Opcode, JsNativeError, JsResult};
 
-use super::{ByteCompiler, NodeKind};
+use super::{ByteCompiler, JumpControlInfoKind, NodeKind};
 
-mod kontinue;
+mod r#continue;
+mod r#try;
 
-pub(crate) use kontinue::compile_continue;
-
+pub(crate) use r#continue::compile_continue;
+pub(crate) use r#try::compile_try;
 pub(crate) fn compile_if<'b>(
     byte_compiler: &mut ByteCompiler<'b>,
     node: &If,
@@ -93,6 +94,116 @@ pub(crate) fn compile_labeled<'b>(
             byte_compiler.function(f.into(), NodeKind::Declaration, false)?;
         }
     }
+
+    Ok(())
+}
+
+pub(crate) fn compile_break<'b>(
+    byte_compiler: &mut ByteCompiler<'b>,
+    node: &Break,
+) -> JsResult<()> {
+    let next = byte_compiler.next_opcode_location();
+    if let Some(info) = byte_compiler
+        .jump_info
+        .last()
+        .filter(|info| info.kind == JumpControlInfoKind::Try)
+    {
+        let in_finally = if let Some(finally_start) = info.finally_start {
+            next >= finally_start.index
+        } else {
+            false
+        };
+        let in_catch_no_finally = !info.has_finally && info.in_catch;
+
+        if in_finally {
+            byte_compiler.emit_opcode(Opcode::PopIfThrown);
+        }
+        if in_finally || in_catch_no_finally {
+            byte_compiler.emit_opcode(Opcode::CatchEnd2);
+        } else {
+            byte_compiler.emit_opcode(Opcode::TryEnd);
+        }
+        byte_compiler.emit(Opcode::FinallySetJump, &[u32::MAX]);
+    }
+    let label = byte_compiler.jump();
+    if let Some(label_name) = node.label() {
+        let mut found = false;
+        for info in byte_compiler.jump_info.iter_mut().rev() {
+            if info.label == Some(label_name) {
+                info.breaks.push(label);
+                found = true;
+                break;
+            }
+        }
+        // TODO: promote to an early error.
+        if !found {
+            return Err(JsNativeError::syntax()
+                .with_message(format!(
+                    "Cannot use the undeclared label '{}'",
+                    byte_compiler.interner().resolve_expect(label_name)
+                ))
+                .into());
+        }
+    } else {
+        byte_compiler
+            .jump_info
+            .last_mut()
+            // TODO: promote to an early error.
+            .ok_or_else(|| {
+                JsNativeError::syntax()
+                    .with_message("unlabeled break must be inside loop or switch")
+            })?
+            .breaks
+            .push(label);
+    }
+
+    Ok(())
+}
+
+pub(crate) fn compile_switch<'b>(
+    byte_compiler: &mut ByteCompiler<'b>,
+    switch: &Switch,
+    configurable_globals: bool,
+) -> JsResult<()> {
+    byte_compiler.context.push_compile_time_environment(false);
+    let push_env = byte_compiler.emit_opcode_with_two_operands(Opcode::PushDeclarativeEnvironment);
+    for case in switch.cases() {
+        byte_compiler.create_decls(case.body(), configurable_globals);
+    }
+    byte_compiler.emit_opcode(Opcode::LoopStart);
+
+    let start_address = byte_compiler.next_opcode_location();
+    byte_compiler.push_switch_control_info(None, start_address);
+
+    byte_compiler.compile_expr(switch.val(), true)?;
+    let mut labels = Vec::with_capacity(switch.cases().len());
+    for case in switch.cases() {
+        byte_compiler.compile_expr(case.condition(), true)?;
+        labels.push(byte_compiler.emit_opcode_with_operand(Opcode::Case));
+    }
+
+    let exit = byte_compiler.emit_opcode_with_operand(Opcode::Default);
+
+    for (label, case) in labels.into_iter().zip(switch.cases()) {
+        byte_compiler.patch_jump(label);
+        byte_compiler.compile_statement_list(case.body(), false, configurable_globals)?;
+    }
+
+    byte_compiler.patch_jump(exit);
+    if let Some(body) = switch.default() {
+        byte_compiler.create_decls(body, configurable_globals);
+        byte_compiler.compile_statement_list(body, false, configurable_globals)?;
+    }
+
+    byte_compiler.pop_switch_control_info();
+
+    byte_compiler.emit_opcode(Opcode::LoopEnd);
+
+    let (num_bindings, compile_environment) = byte_compiler.context.pop_compile_time_environment();
+    let index_compile_environment = byte_compiler.push_compile_environment(compile_environment);
+    byte_compiler.patch_jump_with_target(push_env.0, num_bindings as u32);
+    byte_compiler.patch_jump_with_target(push_env.1, index_compile_environment as u32);
+    byte_compiler.emit_opcode(Opcode::PopEnvironment);
 
     Ok(())
 }
