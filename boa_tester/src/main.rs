boa-dev/boa

commit 9c7b4d55316bfb8f118d413c33467e29eb38268d

Cleanup `boa_tester` (#2440)
Just some quality changes to improve the maintainability of the tester:

- Replaces `anyhow` with `color_eyre` to have a better output on errors/panics.
- Changes the ignore file to a TOML file and replaces all parsing logic with the `toml` crate.
- Adds a `ignored` field on all `Test`s to simplify run logic.
- Replaces the global `IGNORED` with an `ignored` argument on the CLI.

boa_tester/src/main.rs
@@ -1,7 +1,7 @@
 //! Test262 test runner
 //!
 //! This crate will run the full ECMAScript test suite (Test262) and report compliance of the
-//! `boa` context.
+//! `boa` wrap_err.
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/boa-dev/boa/main/assets/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/boa-dev/boa/main/assets/logo.svg"
@@ -70,26 +70,34 @@ use self::{
     read::{read_harness, read_suite, read_test, MetaData, Negative, TestFlag},
     results::{compare_results, write_json},
 };
-use anyhow::{bail, Context};
 use bitflags::bitflags;
 use clap::{ArgAction, Parser, ValueHint};
+use color_eyre::{
+    eyre::{bail, WrapErr},
+    Result,
+};
 use colored::Colorize;
 use fxhash::{FxHashMap, FxHashSet};
-use once_cell::sync::Lazy;
 use read::ErrorType;
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{Unexpected, Visitor},
+    Deserialize, Deserializer, Serialize,
+};
 use std::{
-    fs,
+    fs::{self, File},
+    io::Read,
     path::{Path, PathBuf},
 };
 
 /// Structure to allow defining ignored tests, features and files that should
 /// be ignored even when reading.
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 struct Ignored {
+    #[serde(default)]
     tests: FxHashSet<Box<str>>,
+    #[serde(default)]
     features: FxHashSet<Box<str>>,
-    files: FxHashSet<Box<str>>,
+    #[serde(default = "TestFlags::empty")]
     flags: TestFlags,
 }
 
@@ -102,16 +110,17 @@ impl Ignored {
 
     /// Checks if the ignore list contains the given feature name in the list
     /// of features to ignore.
-    pub(crate) fn contains_any_feature(&self, features: &[Box<str>]) -> bool {
-        features
-            .iter()
-            .any(|feature| self.features.contains(feature))
-    }
-
-    /// Checks if the ignore list contains the given file name in the list to
-    /// ignore from reading.
-    pub(crate) fn contains_file(&self, file: &str) -> bool {
-        self.files.contains(file)
+    pub(crate) fn contains_feature(&self, feature: &str) -> bool {
+        if self.features.contains(feature) {
+            return true;
+        }
+        // Some features are an accessor instead of a simple feature name e.g. `Intl.DurationFormat`.
+        // This ensures those are also ignored.
+        feature
+            .split('.')
+            .next()
+            .map(|feat| self.features.contains(feat))
+            .unwrap_or_default()
     }
 
     pub(crate) fn contains_any_flag(&self, flags: TestFlags) -> bool {
@@ -124,73 +133,10 @@ impl Default for Ignored {
         Self {
             tests: FxHashSet::default(),
             features: FxHashSet::default(),
-            files: FxHashSet::default(),
             flags: TestFlags::empty(),
         }
     }
 }
-
-/// List of ignored tests.
-static IGNORED: Lazy<Ignored> = Lazy::new(|| {
-    let path = Path::new("test_ignore.txt");
-    if path.exists() {
-        let filtered = fs::read_to_string(path).expect("could not read test filters");
-        filtered
-            .lines()
-            .filter(|line| !line.is_empty() && !line.starts_with("//"))
-            .fold(Ignored::default(), |mut ign, line| {
-                // let mut line = line.to_owned();
-                if line.starts_with("file:") {
-                    let file = line
-                        .strip_prefix("file:")
-                        .expect("prefix disappeared")
-                        .trim()
-                        .to_owned()
-                        .into_boxed_str();
-                    let test = if file.ends_with(".js") {
-                        file.strip_suffix(".js")
-                            .expect("suffix disappeared")
-                            .to_owned()
-                            .into_boxed_str()
-                    } else {
-                        file.clone()
-                    };
-                    ign.files.insert(file);
-                    ign.tests.insert(test);
-                } else if line.starts_with("feature:") {
-                    ign.features.insert(
-                        line.strip_prefix("feature:")
-                            .expect("prefix disappeared")
-                            .trim()
-                            .to_owned()
-                            .into_boxed_str(),
-                    );
-                } else if line.starts_with("flag:") {
-                    let flag = line
-                        .strip_prefix("flag:")
-                        .expect("prefix disappeared")
-                        .trim()
-                        .parse::<TestFlag>()
-                        .expect("invalid flag found");
-                    ign.flags.insert(flag.into());
-                } else {
-                    let mut test = line.trim();
-                    if test
-                        .rsplit('.')
-                        .next()
-                        .map(|ext| ext.eq_ignore_ascii_case("js"))
-                        == Some(true)
-                    {
-                        test = test.strip_suffix(".js").expect("suffix disappeared");
-                    }
-                    ign.tests.insert(test.to_owned().into_boxed_str());
-                }
-                ign
-            })
-    } else {
-        Ignored::default()
-    }
-});
 
 /// Boa test262 tester
 #[derive(Debug, Parser)]
@@ -217,6 +163,10 @@ enum Cli {
         /// Execute tests serially
         #[arg(short, long)]
         disable_parallelism: bool,
+
+        /// Path to a TOML file with the ignored tests, features, flags and/or files.
+        #[arg(short, long, default_value = "test_ignore.toml", value_hint = ValueHint::FilePath)]
+        ignored: PathBuf,
     },
     /// Compare two test suite results.
     Compare {
@@ -235,7 +185,8 @@ enum Cli {
 }
 
 /// Program entry point.
-fn main() {
+fn main() -> Result<()> {
+    color_eyre::install()?;
     match Cli::parse() {
         Cli::Run {
             verbose,
@@ -243,23 +194,15 @@ fn main() {
             suite,
             output,
             disable_parallelism,
-        } => {
-            if let Err(e) = run_test_suite(
-                verbose,
-                !disable_parallelism,
-                test262_path.as_path(),
-                suite.as_path(),
-                output.as_deref(),
-            ) {
-                eprintln!("Error: {e}");
-                let mut src = e.source();
-                while let Some(e) = src {
-                    eprintln!("    caused by: {e}");
-                    src = e.source();
-                }
-                std::process::exit(1);
-            }
-        }
+            ignored: ignore,
+        } => run_test_suite(
+            verbose,
+            !disable_parallelism,
+            test262_path.as_path(),
+            suite.as_path(),
+            output.as_deref(),
+            ignore.as_path(),
+        ),
         Cli::Compare {
             base,
             new,
@@ -275,24 +218,33 @@ fn run_test_suite(
     test262_path: &Path,
     suite: &Path,
     output: Option<&Path>,
-) -> anyhow::Result<()> {
+    ignored: &Path,
+) -> Result<()> {
     if let Some(path) = output {
         if path.exists() {
             if !path.is_dir() {
                 bail!("the output path must be a directory.");
             }
         } else {
-            fs::create_dir_all(path).context("could not create the output directory")?;
+            fs::create_dir_all(path).wrap_err("could not create the output directory")?;
         }
     }
+
+    let ignored = {
+        let mut input = String::new();
+        let mut f = File::open(ignored).wrap_err("could not open ignored tests file")?;
+        f.read_to_string(&mut input)
+            .wrap_err("could not read ignored tests file")?;
+        toml::from_str(&input).wrap_err("could not decode ignored tests file")?
+    };
 
     if verbose != 0 {
         println!("Loading the test suite...");
     }
-    let harness = read_harness(test262_path).context("could not read harness")?;
+    let harness = read_harness(test262_path).wrap_err("could not read harness")?;
 
     if suite.to_string_lossy().ends_with(".js") {
-        let test = read_test(&test262_path.join(suite)).with_context(|| {
+        let test = read_test(&test262_path.join(suite)).wrap_err_with(|| {
             let suite = suite.display();
             format!("could not read the test {suite}")
         })?;
@@ -304,7 +256,7 @@ fn run_test_suite(
 
         println!();
     } else {
-        let suite = read_suite(&test262_path.join(suite)).with_context(|| {
+        let suite = read_suite(&test262_path.join(suite), &ignored, false).wrap_err_with(|| {
             let suite = suite.display();
             format!("could not read the suite {suite}")
         })?;
@@ -332,7 +284,7 @@ fn run_test_suite(
         );
 
         write_json(results, output, verbose)
-            .context("could not write the results to the output JSON file")?;
+            .wrap_err("could not write the results to the output JSON file")?;
     }
 
     Ok(())
@@ -419,6 +371,7 @@ struct Test {
     includes: Box<[Box<str>]>,
     locale: Locale,
     content: Box<str>,
+    ignored: bool,
 }
 
 impl Test {
@@ -440,15 +393,12 @@ impl Test {
             includes: metadata.includes,
             locale: metadata.locale,
             content: content.into(),
+            ignored: false,
         }
     }
 
-    /// Sets the name of the test.
-    fn set_name<N>(&mut self, name: N)
-    where
-        N: Into<Box<str>>,
-    {
-        self.name = name.into();
+    fn set_ignored(&mut self) {
+        self.ignored = true;
     }
 }
 
@@ -530,6 +480,59 @@ where
             }
 
             result
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for TestFlags {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct FlagsVisitor;
+
+        impl<'de> Visitor<'de> for FlagsVisitor {
+            type Value = TestFlags;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(formatter, "a sequence of flags")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let mut flags = TestFlags::empty();
+                while let Some(elem) = seq.next_element::<TestFlag>()? {
+                    flags |= elem.into();
+                }
+                Ok(flags)
+            }
+        }
+
+        struct RawFlagsVisitor;
+
+        impl<'de> Visitor<'de> for RawFlagsVisitor {
+            type Value = TestFlags;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(formatter, "a flags number")
+            }
+
+            fn visit_u16<E>(self, v: u16) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                TestFlags::from_bits(v).ok_or_else(|| {
+                    E::invalid_value(Unexpected::Unsigned(v.into()), &"a valid flag number")
+                })
+            }
+        }
+
+        if deserializer.is_human_readable() {
+            deserializer.deserialize_seq(FlagsVisitor)
+        } else {
+            deserializer.deserialize_u16(RawFlagsVisitor)
         }
     }
 }

boa_tester/Cargo.toml
@@ -26,4 +26,5 @@ once_cell = "1.16.0"
 colored = "2.0.0"
 fxhash = "0.2.1"
 rayon = "1.5.3"
-anyhow = "1.0.66"
+toml = "0.5.9"
+color-eyre = "0.6.2"

boa_tester/src/exec/mod.rs
@@ -2,11 +2,12 @@
 
 mod js262;
 
+use std::borrow::Cow;
+
 use crate::read::ErrorType;
 
 use super::{
-    Harness, Outcome, Phase, SuiteResult, Test, TestFlags, TestOutcomeResult, TestResult,
-    TestSuite, IGNORED,
+    Harness, Outcome, Phase, SuiteResult, Test, TestFlags, TestOutcomeResult, TestResult, TestSuite,
 };
 use boa_engine::{
     builtins::JsArgs, object::FunctionBuilder, property::Attribute, Context, JsNativeErrorKind,
@@ -130,180 +131,7 @@ impl Test {
 
     /// Runs the test once, in strict or non-strict mode
     fn run_once(&self, harness: &Harness, strict: bool, verbose: u8) -> TestResult {
-        if verbose > 1 {
-            println!(
-                "`{}`{}: starting",
-                self.name,
-                if strict { " (strict mode)" } else { "" }
-            );
-        }
-
-        let test_content = if strict {
-            format!("\"use strict\";\n{}", self.content)
-        } else {
-            self.content.to_string()
-        };
-
-        let (result, result_text) = if !IGNORED.contains_any_flag(self.flags)
-            && !IGNORED.contains_test(&self.name)
-            && !IGNORED.contains_any_feature(&self.features)
-            && (matches!(self.expected_outcome, Outcome::Positive)
-                || matches!(
-                    self.expected_outcome,
-                    Outcome::Negative {
-                        phase: Phase::Parse,
-                        error_type: _,
-                    }
-                )
-                || matches!(
-                    self.expected_outcome,
-                    Outcome::Negative {
-                        phase: Phase::Early,
-                        error_type: _,
-                    }
-                )
-                || matches!(
-                    self.expected_outcome,
-                    Outcome::Negative {
-                        phase: Phase::Runtime,
-                        error_type: _,
-                    }
-                )) {
-            let res = std::panic::catch_unwind(|| match self.expected_outcome {
-                Outcome::Positive => {
-                    let mut context = Context::default();
-                    let async_result = AsyncResult::default();
-
-                    if let Err(e) = self.set_up_env(harness, &mut context, async_result.clone()) {
-                        return (false, e);
-                    }
-
-                    // TODO: timeout
-                    let value = match context.eval(&test_content) {
-                        Ok(v) => v,
-                        Err(e) => return (false, format!("Uncaught {e}")),
-                    };
-
-                    if let Err(e) = async_result.inner.borrow().as_ref() {
-                        return (false, format!("Uncaught {e}"));
-                    }
-
-                    (true, value.display().to_string())
-                }
-                Outcome::Negative {
-                    phase: Phase::Parse | Phase::Early,
-                    error_type,
-                } => {
-                    assert_eq!(
-                        error_type,
-                        ErrorType::SyntaxError,
-                        "non-SyntaxError parsing/early error found in {}",
-                        self.name
-                    );
-
-                    let mut context = Context::default();
-                    match context.parse(&test_content) {
-                        Ok(statement_list) => match context.compile(&statement_list) {
-                            Ok(_) => (false, "StatementList compilation should fail".to_owned()),
-                            Err(e) => (true, format!("Uncaught {e:?}")),
-                        },
-                        Err(e) => (true, format!("Uncaught {e}")),
-                    }
-                }
-                Outcome::Negative {
-                    phase: Phase::Resolution,
-                    error_type: _,
-                } => todo!("check module resolution errors"),
-                Outcome::Negative {
-                    phase: Phase::Runtime,
-                    error_type,
-                } => {
-                    let mut context = Context::default();
-                    if let Err(e) = self.set_up_env(harness, &mut context, AsyncResult::default()) {
-                        return (false, e);
-                    }
-                    let code = match Parser::new(test_content.as_bytes())
-                        .parse_all(context.interner_mut())
-                        .map_err(Into::into)
-                        .and_then(|stmts| context.compile(&stmts))
-                    {
-                        Ok(code) => code,
-                        Err(e) => return (false, format!("Uncaught {e}")),
-                    };
-
-                    // TODO: timeout
-                    let e = match context.execute(code) {
-                        Ok(res) => return (false, res.display().to_string()),
-                        Err(e) => e,
-                    };
-                    if let Ok(e) = e.try_native(&mut context) {
-                        match &e.kind {
-                            JsNativeErrorKind::Syntax if error_type == ErrorType::SyntaxError => {}
-                            JsNativeErrorKind::Reference
-                                if error_type == ErrorType::ReferenceError => {}
-                            JsNativeErrorKind::Range if error_type == ErrorType::RangeError => {}
-                            JsNativeErrorKind::Type if error_type == ErrorType::TypeError => {}
-                            _ => return (false, format!("Uncaught {e}")),
-                        }
-                        (true, format!("Uncaught {e}"))
-                    } else {
-                        let passed = e
-                            .as_opaque()
-                            .expect("try_native cannot fail if e is not opaque")
-                            .as_object()
-                            .and_then(|o| o.get("constructor", &mut context).ok())
-                            .as_ref()
-                            .and_then(JsValue::as_object)
-                            .and_then(|o| o.get("name", &mut context).ok())
-                            .as_ref()
-                            .and_then(JsValue::as_string)
-                            .map(|s| s == error_type.as_str())
-                            .unwrap_or_default();
-                        (passed, format!("Uncaught {e}"))
-                    }
-                }
-            });
-
-            let result = res.map_or_else(
-                |_| {
-                    eprintln!("last panic was on test \"{}\"", self.name);
-                    (TestOutcomeResult::Panic, String::new())
-                },
-                |(res, text)| {
-                    if res {
-                        (TestOutcomeResult::Passed, text)
-                    } else {
-                        (TestOutcomeResult::Failed, text)
-                    }
-                },
-            );
-
-            if verbose > 1 {
-                println!(
-                    "`{}`{}: {}",
-                    self.name,
-                    if strict { " (strict mode)" } else { "" },
-                    if matches!(result, (TestOutcomeResult::Passed, _)) {
-                        "Passed".green()
-                    } else if matches!(result, (TestOutcomeResult::Failed, _)) {
-                        "Failed".red()
-                    } else {
-                        "⚠ Panic ⚠".red()
-                    }
-                );
-            } else {
-                print!(
-                    "{}",
-                    if matches!(result, (TestOutcomeResult::Passed, _)) {
-                        ".".green()
-                    } else {
-                        "F".red()
-                    }
-                );
-            }
-
-            result
-        } else {
+        if self.ignored {
             if verbose > 1 {
                 println!(
                     "`{}`{}: {}",
@@ -314,8 +142,159 @@ impl Test {
             } else {
                 print!("{}", "-".yellow());
             }
-            (TestOutcomeResult::Ignored, String::new())
+            return TestResult {
+                name: self.name.clone(),
+                strict,
+                result: TestOutcomeResult::Ignored,
+                result_text: Box::default(),
+            };
+        }
+        if verbose > 1 {
+            println!(
+                "`{}`{}: starting",
+                self.name,
+                if strict { " (strict mode)" } else { "" }
+            );
+        }
+
+        let test_content = if strict {
+            Cow::Owned(format!("\"use strict\";\n{}", self.content))
+        } else {
+            Cow::Borrowed(&*self.content)
         };
+
+        let result = std::panic::catch_unwind(|| match self.expected_outcome {
+            Outcome::Positive => {
+                let mut context = Context::default();
+                let async_result = AsyncResult::default();
+
+                if let Err(e) = self.set_up_env(harness, &mut context, async_result.clone()) {
+                    return (false, e);
+                }
+
+                // TODO: timeout
+                let value = match context.eval(&*test_content) {
+                    Ok(v) => v,
+                    Err(e) => return (false, format!("Uncaught {e}")),
+                };
+
+                if let Err(e) = async_result.inner.borrow().as_ref() {
+                    return (false, format!("Uncaught {e}"));
+                }
+
+                (true, value.display().to_string())
+            }
+            Outcome::Negative {
+                phase: Phase::Parse | Phase::Early,
+                error_type,
+            } => {
+                assert_eq!(
+                    error_type,
+                    ErrorType::SyntaxError,
+                    "non-SyntaxError parsing/early error found in {}",
+                    self.name
+                );
+
+                let mut context = Context::default();
+                match context.parse(&*test_content) {
+                    Ok(statement_list) => match context.compile(&statement_list) {
+                        Ok(_) => (false, "StatementList compilation should fail".to_owned()),
+                        Err(e) => (true, format!("Uncaught {e:?}")),
+                    },
+                    Err(e) => (true, format!("Uncaught {e}")),
+                }
+            }
+            Outcome::Negative {
+                phase: Phase::Resolution,
+                error_type: _,
+            } => todo!("check module resolution errors"),
+            Outcome::Negative {
+                phase: Phase::Runtime,
+                error_type,
+            } => {
+                let mut context = Context::default();
+                if let Err(e) = self.set_up_env(harness, &mut context, AsyncResult::default()) {
+                    return (false, e);
+                }
+                let code = match Parser::new(test_content.as_bytes())
+                    .parse_all(context.interner_mut())
+                    .map_err(Into::into)
+                    .and_then(|stmts| context.compile(&stmts))
+                {
+                    Ok(code) => code,
+                    Err(e) => return (false, format!("Uncaught {e}")),
+                };
+
+                // TODO: timeout
+                let e = match context.execute(code) {
+                    Ok(res) => return (false, res.display().to_string()),
+                    Err(e) => e,
+                };
+                if let Ok(e) = e.try_native(&mut context) {
+                    match &e.kind {
+                        JsNativeErrorKind::Syntax if error_type == ErrorType::SyntaxError => {}
+                        JsNativeErrorKind::Reference if error_type == ErrorType::ReferenceError => {
+                        }
+                        JsNativeErrorKind::Range if error_type == ErrorType::RangeError => {}
+                        JsNativeErrorKind::Type if error_type == ErrorType::TypeError => {}
+                        _ => return (false, format!("Uncaught {e}")),
+                    }
+                    (true, format!("Uncaught {e}"))
+                } else {
+                    let passed = e
+                        .as_opaque()
+                        .expect("try_native cannot fail if e is not opaque")
+                        .as_object()
+                        .and_then(|o| o.get("constructor", &mut context).ok())
+                        .as_ref()
+                        .and_then(JsValue::as_object)
+                        .and_then(|o| o.get("name", &mut context).ok())
+                        .as_ref()
+                        .and_then(JsValue::as_string)
+                        .map(|s| s == error_type.as_str())
+                        .unwrap_or_default();
+                    (passed, format!("Uncaught {e}"))
+                }
+            }
+        });
+
+        let (result, result_text) = result.map_or_else(
+            |_| {
+                eprintln!("last panic was on test \"{}\"", self.name);
+                (TestOutcomeResult::Panic, String::new())
+            },
+            |(res, text)| {
+                if res {
+                    (TestOutcomeResult::Passed, text)
+                } else {
+                    (TestOutcomeResult::Failed, text)
+                }
+            },
+        );
+
+        if verbose > 1 {
+            println!(
+                "`{}`{}: {}",
+                self.name,
+                if strict { " (strict mode)" } else { "" },
+                if result == TestOutcomeResult::Passed {
+                    "Passed".green()
+                } else if result == TestOutcomeResult::Failed {
+                    "Failed".red()
+                } else {
+                    "⚠ Panic ⚠".red()
+                }
+            );
+        } else {
+            print!(
+                "{}",
+                if result == TestOutcomeResult::Passed {
+                    ".".green()
+                } else {
+                    "F".red()
+                }
+            );
+        }
 
         if verbose > 2 {
             println!(

commit 5125da00082d73b28526b9b4822e7d69f356c90f

Fixed typo in the docs (#2450)
This PR fixes a typo introduced in #2440

boa_tester/src/main.rs
@@ -1,7 +1,7 @@
 //! Test262 test runner
 //!
 //! This crate will run the full ECMAScript test suite (Test262) and report compliance of the
-//! `boa` wrap_err.
+//! `boa` engine.
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/boa-dev/boa/main/assets/logo.svg",