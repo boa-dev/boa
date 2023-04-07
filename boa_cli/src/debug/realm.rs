3bbc6128834c2b427d6dba4796712b82580ed375
jedel1043 <jedel0124@gmail.com>
2023-04-07 11:05:35 -0600

    Apply suggestions

--- boa_cli/src/debug/realm.rs
+++ boa_cli/src/debug/realm.rs
@@ -1,7 +1,7 @@
 use boa_engine::{object::ObjectInitializer, Context, JsObject, JsResult, JsValue, NativeFunction};
 
 /// Creates a new ECMAScript Realm and returns the global object of the realm.
-fn new_realm(_: &JsValue, _: &[JsValue], _: &mut Context<'_>) -> JsResult<JsValue> {
+fn create(_: &JsValue, _: &[JsValue], _: &mut Context<'_>) -> JsResult<JsValue> {
     let context = &mut Context::default();
 
     Ok(context.global_object().into())
@@ -9,6 +9,6 @@ fn new_realm(_: &JsValue, _: &[JsValue], _: &mut Context<'_>) -> JsResult<JsValu
 
 pub(super) fn create_object(context: &mut Context<'_>) -> JsObject {
     ObjectInitializer::new(context)
-        .function(NativeFunction::from_fn_ptr(new_realm), "newRealm", 0)
+        .function(NativeFunction::from_fn_ptr(create), "create", 0)
         .build()
 }
