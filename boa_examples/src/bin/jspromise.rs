bd61b6b837c23310feeb2f2b1efdc04e30aeb2d5
Christopher Navarro <chrisidxloop@gmail.com>
2023-04-12 19:03:44 -0400

    updated jspromise example with comments

--- boa_examples/src/bin/jspromise.rs
+++ boa_examples/src/bin/jspromise.rs
@@ -1,3 +1,16 @@
+/// Creates a new promise object from an executor function.
+///
+/// It is equivalent to calling the [`Promise()`] constructor, which makes it share the same
+/// execution semantics as the constructor:
+/// - The executor function `executor` is called synchronously just after the promise is created.
+/// - The executor return value is ignored.
+/// - Any error thrown within the execution of `executor` will call the `reject` function
+/// of the newly created promise, unless either `resolve` or `reject` were already called
+/// beforehand.
+///
+/// `executor` receives as an argument the [`ResolvingFunctions`] needed to settle the promise,
+/// which can be done by either calling the `resolve` function or the `reject` function.
+///
 use std::error::Error;
 use boa_engine::{
 job::SimpleJobQueue,
