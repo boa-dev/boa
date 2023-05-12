cbaaf624b25c38f9ccb7923734e3bb5cc94c33a6
jedel1043 <jedel0124@gmail.com>
2023-05-11 19:08:37 -0600

    npx prettier

--- boa_examples/scripts/modules/trig.mjs
+++ boa_examples/scripts/modules/trig.mjs
@@ -1,11 +1,11 @@
 import { sum, mult, sqrt } from "./operations.mjs";
 
 function pyth(a, b) {
-    let a2 = mult(a, a);
-    let b2 = mult(b, b);
-    let a2b2 = sum(a2, b2);
+  let a2 = mult(a, a);
+  let b2 = mult(b, b);
+  let a2b2 = sum(a2, b2);
 
-    return sqrt(a2b2);
+  return sqrt(a2b2);
 }
 
 export { pyth };
