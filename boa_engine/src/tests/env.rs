253c81c83194c3c3479a86ef240b29b224991d4e
jedel1043 <jedel0124@gmail.com>
2023-05-30 17:43:24 -0600

    Add test case for issue #2719

--- boa_engine/src/tests/env.rs
+++ boa_engine/src/tests/env.rs
@@ -1,6 +1,6 @@
 use indoc::indoc;
 
-use crate::{run_test_actions, TestAction};
+use crate::{run_test_actions, JsNativeErrorKind, TestAction};
 
 #[test]
 // https://github.com/boa-dev/boa/issues/2317
@@ -29,4 +29,20 @@ fn fun_block_eval_2317() {
             "defaultinner",
         ),
     ]);
+}
+
+#[test]
+// https://github.com/boa-dev/boa/issues/2719
+fn with_env_not_panic() {
+    run_test_actions([TestAction::assert_native_error(
+        indoc! {r#"
+            with({ p1:1,  }) {k[oa>>2]=d;}
+            {
+            let a12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 = 1,
+                b = "";
+            }
+        "#},
+        JsNativeErrorKind::Reference,
+        "k is not defined",
+    )]);
 }
