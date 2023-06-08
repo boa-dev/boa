493176b4f8f633515e36b36639dca3d14562c6b5
raskad <32105367+raskad@users.noreply.github.com>
2023-06-08 14:56:21 +0200

    Return the correct value during a labelled break (#2996)

--- boa_engine/src/bytecompiler/statement/break.rs
+++ boa_engine/src/bytecompiler/statement/break.rs
@@ -8,6 +8,12 @@ use boa_interner::Sym;
 impl ByteCompiler<'_, '_> {
     /// Compile a [`Break`] `boa_ast` node
     pub(crate) fn compile_break(&mut self, node: Break) {
+        let opcode = if node.label().is_some() {
+            Opcode::BreakLabel
+        } else {
+            Opcode::Break
+        };
+
         if let Some(info) = self.jump_info.last().filter(|info| info.is_try_block()) {
             let in_finally = info.in_finally();
             let in_catch_no_finally = info.in_catch() && !info.has_finally();
@@ -20,8 +26,7 @@ impl ByteCompiler<'_, '_> {
                 self.emit_opcode(Opcode::CatchEnd2);
             }
 
-            let (break_label, target_jump_label) =
-                self.emit_opcode_with_two_operands(Opcode::Break);
+            let (break_label, target_jump_label) = self.emit_opcode_with_two_operands(opcode);
 
             if let Some(node_label) = node.label() {
                 self.search_jump_info_label(target_jump_label, node_label);
@@ -48,15 +53,12 @@ impl ByteCompiler<'_, '_> {
         }
 
         // Emit the break opcode -> (Label, Label)
-        let (break_label, target_label) = self.emit_opcode_with_two_operands(Opcode::Break);
-        if node.label().is_some() {
-            self.search_jump_info_label(
-                break_label,
-                node.label().expect("must exist in this block"),
-            );
-            self.search_jump_info_label(target_label, node.label().expect("must exist"));
+        let (break_label, target_label) = self.emit_opcode_with_two_operands(opcode);
+        if let Some(label) = node.label() {
+            self.search_jump_info_label(break_label, label);
+            self.search_jump_info_label(target_label, label);
             return;
-        };
+        }
 
         let info = self
             .jump_info
