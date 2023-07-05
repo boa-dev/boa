5eb8541e4a9ba90b6cf694d3f89bf59c2ebad156
Jason Williams <936006+jasonwilliams@users.noreply.github.com>
2023-07-05 19:35:12 +0100

    Changelog17 (#3101)
    * v0.17.0 changelog
    
    * Added latest changes, updated dependencies
    
    ---------
    
    Co-authored-by: Iban Eguia Moraza <razican@protonmail.ch>

--- boa_engine/benches/bench_scripts/boolean_object_access.js
+++ boa_engine/benches/bench_scripts/boolean_object_access.js
@@ -1,7 +1,7 @@
 new Boolean(
   !new Boolean(
     new Boolean(
-      !new Boolean(false).valueOf() && new Boolean(true).valueOf()
-    ).valueOf()
-  ).valueOf()
+      !new Boolean(false).valueOf() && new Boolean(true).valueOf(),
+    ).valueOf(),
+  ).valueOf(),
 ).valueOf();
