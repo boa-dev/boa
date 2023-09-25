a4de8f9f3da5961ac17448a1029ea991fe4ee13e
jedel1043 <jedel0124@gmail.com>
2023-09-25 15:29:28 -0600

    Change deny lints to warn lints

--- boa_ast/src/lib.rs
+++ boa_ast/src/lib.rs
@@ -18,8 +18,7 @@
     html_favicon_url = "https://raw.githubusercontent.com/boa-dev/boa/main/assets/logo.svg"
 )]
 #![cfg_attr(not(test), forbid(clippy::unwrap_used))]
-#![warn(missing_docs, clippy::dbg_macro)]
-#![deny(
+#![warn(
     // rustc lint groups https://doc.rust-lang.org/rustc/lints/groups.html
     warnings,
     future_incompatible,
@@ -31,6 +30,7 @@
     unused,
 
     // rustc allowed-by-default lints https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html
+    missing_docs,
     macro_use_extern_crate,
     meta_variable_misuse,
     missing_abi,
@@ -58,6 +58,9 @@
     rustdoc::invalid_codeblock_attributes,
     rustdoc::invalid_rust_codeblocks,
     rustdoc::bare_urls,
+
+    // clippy allowed by default
+    clippy::dbg_macro,
 
     // clippy categories https://doc.rust-lang.org/clippy/
     clippy::all,
