ef842f821af8df03cfacb4604c9f456aac992cbd
Hans Larsen <hans@larsen.online>
2024-04-02 15:10:21 -0700

    prettier

--- core/engine/tests/assets/dir1/file1_1.js
+++ core/engine/tests/assets/dir1/file1_1.js
@@ -1,5 +1,5 @@
-import {file1_2} from './file1_2.js';
+import { file1_2 } from "./file1_2.js";
 
 export function file1_1() {
-    return 'file1_1' + '.' + file1_2();
+  return "file1_1" + "." + file1_2();
 }
--- core/engine/tests/assets/dir1/file1_2.js
+++ core/engine/tests/assets/dir1/file1_2.js
@@ -1,3 +1,3 @@
 export function file1_2() {
-    return 'file1_2';
+  return "file1_2";
 }
