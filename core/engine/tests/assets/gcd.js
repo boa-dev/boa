8ada70c5122cc297389bd715c4579ff1e86b638a
Hans Larsen <hans@larsen.online>
2024-07-29 22:34:55 +0100

    clippies and fmt

--- core/engine/tests/assets/gcd.js
+++ core/engine/tests/assets/gcd.js
@@ -6,21 +6,21 @@
  * @throws {TypeError} If either {a} or {b} is not finite.
  */
 export function gcd(a, b) {
-    a = +a;
-    b = +b;
-    if (!Number.isFinite(a) || !Number.isFinite(b)) {
-        throw new TypeError('Invalid input');
-    }
+  a = +a;
+  b = +b;
+  if (!Number.isFinite(a) || !Number.isFinite(b)) {
+    throw new TypeError("Invalid input");
+  }
 
-    // Euclidean algorithm
-    function inner_gcd(a, b) {
-        while (b !== 0) {
-            let t = b;
-            b = a % b;
-            a = t;
-        }
-        return a;
+  // Euclidean algorithm
+  function inner_gcd(a, b) {
+    while (b !== 0) {
+      let t = b;
+      b = a % b;
+      a = t;
     }
+    return a;
+  }
 
-    return inner_gcd(a, b);
+  return inner_gcd(a, b);
 }
