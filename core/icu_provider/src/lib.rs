6c24d9ce0c70d5b1d81f4ca77306426a3d9d2496
jedel1043 <jedel0124@gmail.com>
2024-08-12 01:41:29 -0600

    Split default icu data into lazily deserialized parts

--- core/icu_provider/src/lib.rs
+++ core/icu_provider/src/lib.rs
@@ -21,24 +21,82 @@
 )]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use icu_provider_adapters::fallback::LocaleFallbackProvider;
+use icu_provider::{BufferMarker, BufferProvider, DataError, DataErrorKind, DataKey, DataResponse};
+use icu_provider_adapters::{fallback::LocaleFallbackProvider, fork::MultiForkByKeyProvider};
 use icu_provider_blob::BlobDataProvider;
-use once_cell::sync::Lazy;
+use once_cell::sync::{Lazy, OnceCell};
+
+/// A buffer provider that is lazily deserialized at the first data request.
+///
+/// The provider must specify the list of keys it supports, to avoid deserializing the
+/// buffer for unknown keys.
+struct LazyBufferProvider {
+    provider: OnceCell<BlobDataProvider>,
+    bytes: &'static [u8],
+    valid_keys: &'static [DataKey],
+}
+
+impl BufferProvider for LazyBufferProvider {
+    fn load_buffer(
+        &self,
+        key: DataKey,
+        req: icu_provider::DataRequest<'_>,
+    ) -> Result<DataResponse<BufferMarker>, DataError> {
+        if !self.valid_keys.contains(&key) {
+            return Err(DataErrorKind::MissingDataKey.with_key(key));
+        }
+
+        let Ok(provider) = self
+            .provider
+            .get_or_try_init(|| BlobDataProvider::try_new_from_static_blob(self.bytes))
+        else {
+            return Err(DataErrorKind::Custom.with_str_context("invalid blob data provider"));
+        };
+
+        provider.load_buffer(key, req)
+    }
+}
+
+/// A macro that creates a [`LazyBufferProvider`] from an icu4x crate.
+macro_rules! provider_from_icu_crate {
+    ($service:path) => {
+        paste::paste! {
+            LazyBufferProvider {
+                provider: OnceCell::new(),
+                bytes: include_bytes!(concat!(
+                    env!("CARGO_MANIFEST_DIR"),
+                    "/data/",
+                    stringify!($service),
+                    ".postcard",
+                )),
+                valid_keys: $service::provider::KEYS,
+            }
+        }
+    };
+}
+
+/// Boa's default buffer provider.
+static PROVIDER: Lazy<LocaleFallbackProvider<MultiForkByKeyProvider<LazyBufferProvider>>> =
+    Lazy::new(|| {
+        let provider = MultiForkByKeyProvider::new(vec![
+            provider_from_icu_crate!(icu_casemap),
+            provider_from_icu_crate!(icu_collator),
+            provider_from_icu_crate!(icu_datetime),
+            provider_from_icu_crate!(icu_decimal),
+            provider_from_icu_crate!(icu_list),
+            provider_from_icu_crate!(icu_locid_transform),
+            provider_from_icu_crate!(icu_normalizer),
+            provider_from_icu_crate!(icu_plurals),
+            provider_from_icu_crate!(icu_segmenter),
+        ]);
+        LocaleFallbackProvider::try_new_with_buffer_provider(provider)
+            .expect("The statically compiled data file should be valid.")
+    });
 
 /// Gets the default data provider stored as a [`BufferProvider`].
 ///
 /// [`BufferProvider`]: icu_provider::BufferProvider
 #[must_use]
-pub fn buffer() -> &'static impl icu_provider::BufferProvider {
-    static PROVIDER: Lazy<LocaleFallbackProvider<BlobDataProvider>> = Lazy::new(|| {
-        let blob = BlobDataProvider::try_new_from_static_blob(include_bytes!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/data/icudata.postcard"
-        )))
-        .expect("The statically compiled data file should be valid.");
-        LocaleFallbackProvider::try_new_with_buffer_provider(blob)
-            .expect("The statically compiled data file should be valid.")
-    });
-
+pub fn buffer() -> &'static impl BufferProvider {
     &*PROVIDER
 }
