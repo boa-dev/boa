f6c670ace1b9d7e0640872ca1e5f293331365dbf
Hans Larsen <hans@larsen.online>
2024-08-15 10:58:30 -0700

    dont comment your code while drunk

--- core/string/src/display.rs
+++ core/string/src/display.rs
@@ -12,7 +12,7 @@ pub struct JsStringDisplayEscaped<'a> {
 impl fmt::Display for JsStringDisplayEscaped<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.inner.variant() {
-            // SAFETY: `JsStrVariant::Latin1` is always valid latin1, so no need to check.
+            // SAFETY: `JsStrVariant::Latin1` is always valid utf8, so no need to check.
             JsStrVariant::Latin1(v) => unsafe { std::str::from_utf8_unchecked(v) }.fmt(f),
             JsStrVariant::Utf16(v) => {
                 char::decode_utf16(v.iter().copied()).try_for_each(|r| match r {
