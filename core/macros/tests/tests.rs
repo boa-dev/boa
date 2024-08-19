9a0113fc9e256adbee70eae82a780de29b5797cb
Hans Larsen <hans@larsen.online>
2024-08-18 19:08:38 -0700

    Latin1 is the wrong optimization

--- core/macros/tests/tests.rs
+++ core/macros/tests/tests.rs
@@ -1,6 +1,7 @@
 #![allow(unused_crate_dependencies)]
 
-use boa_macros::utf16;
+use boa_engine::{JsStr, JsString};
+use boa_macros::{js_str, utf16};
 
 #[test]
 fn literal() {
@@ -14,4 +15,16 @@ fn utf16() {
     let utf16 = utf16!("hello!😁😁😁");
     let manual = "hello!😁😁😁".encode_utf16().collect::<Vec<_>>();
     assert_eq!(manual, utf16);
+}
+
+#[test]
+fn latin1_is_wrong() {
+    const NON_UTF8_LATIN1: JsStr<'_> = js_str!("Hello é World!");
+    assert!(NON_UTF8_LATIN1.is_latin1());
+
+    let js_string = JsString::from(NON_UTF8_LATIN1);
+    assert_eq!(
+        format!("{}", js_string.to_std_string_escaped()),
+        "Hello é World!"
+    );
 }
