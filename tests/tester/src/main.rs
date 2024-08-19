4fe47217a8d47b8ced7e40afa5d07925c13e5920
José Julián Espina <jedel0124@gmail.com>
2024-08-19 09:03:57 +0000

    Add spec edition 15 to the tester (#3957)

--- tests/tester/src/main.rs
+++ tests/tester/src/main.rs
@@ -606,6 +606,7 @@ struct VersionedStats {
     es12: Statistics,
     es13: Statistics,
     es14: Statistics,
+    es15: Statistics,
 }
 
 impl<'de> Deserialize<'de> for VersionedStats {
@@ -626,6 +627,8 @@ impl<'de> Deserialize<'de> for VersionedStats {
             es13: Statistics,
             #[serde(default)]
             es14: Option<Statistics>,
+            #[serde(default)]
+            es15: Option<Statistics>,
         }
 
         let inner = Inner::deserialize(deserializer)?;
@@ -641,8 +644,10 @@ impl<'de> Deserialize<'de> for VersionedStats {
             es12,
             es13,
             es14,
+            es15,
         } = inner;
         let es14 = es14.unwrap_or(es13);
+        let es15 = es15.unwrap_or(es14);
 
         Ok(Self {
             es5,
@@ -655,6 +660,7 @@ impl<'de> Deserialize<'de> for VersionedStats {
             es12,
             es13,
             es14,
+            es15,
         })
     }
 }
@@ -684,6 +690,7 @@ impl VersionedStats {
             SpecEdition::ES12 => self.es12,
             SpecEdition::ES13 => self.es13,
             SpecEdition::ES14 => self.es14,
+            SpecEdition::ES15 => self.es15,
             SpecEdition::ESNext => return None,
         };
         Some(stats)
@@ -703,6 +710,7 @@ impl VersionedStats {
             SpecEdition::ES12 => &mut self.es12,
             SpecEdition::ES13 => &mut self.es13,
             SpecEdition::ES14 => &mut self.es14,
+            SpecEdition::ES15 => &mut self.es15,
             SpecEdition::ESNext => return None,
         };
         Some(stats)
@@ -724,6 +732,7 @@ impl Add for VersionedStats {
             es12: self.es12 + rhs.es12,
             es13: self.es13 + rhs.es13,
             es14: self.es14 + rhs.es14,
+            es15: self.es15 + rhs.es15,
         }
     }
 }
@@ -740,6 +749,7 @@ impl AddAssign for VersionedStats {
         self.es12 += rhs.es12;
         self.es13 += rhs.es13;
         self.es14 += rhs.es14;
+        self.es15 += rhs.es15;
     }
 }
 
