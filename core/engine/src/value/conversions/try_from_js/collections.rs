23dd402d62673890e594081847d6dabebf18caad
Nikita-str <newmynamespace@gmail.com>
2024-09-19 18:39:56 +0300

    `TryFromJs` from `JsMap` for `HashMap` & `BtreeMap`

--- core/engine/src/value/conversions/try_from_js/collections.rs
+++ core/engine/src/value/conversions/try_from_js/collections.rs
@@ -3,6 +3,9 @@
 use std::collections::{BTreeMap, HashMap};
 use std::hash::Hash;
 
+use boa_macros::js_str;
+
+use crate::object::{JsArray, JsMap};
 use crate::value::TryFromJs;
 use crate::{Context, JsNativeError, JsResult, JsValue};
 
@@ -18,6 +21,21 @@ where
                 .into());
         };
 
+        // JsMap case
+        if let Ok(js_map) = JsMap::from_object(object.clone()) {
+            let mut map = Self::default();
+            let f = |key, value, context: &mut _| {
+                map.insert(
+                    K::try_from_js(&key, context)?,
+                    V::try_from_js(&value, context)?,
+                );
+                Ok(())
+            };
+            for_each_elem_in_js_map(js_map, f, context)?;
+            return Ok(map);
+        }
+
+        // key-valued JsObject case:
         let keys = object.__own_property_keys__(context)?;
 
         keys.into_iter()
@@ -47,6 +65,21 @@ where
                 .into());
         };
 
+        // JsMap case
+        if let Ok(js_map) = JsMap::from_object(object.clone()) {
+            let mut map = Self::default();
+            let f = |key, value, context: &mut _| {
+                map.insert(
+                    K::try_from_js(&key, context)?,
+                    V::try_from_js(&value, context)?,
+                );
+                Ok(())
+            };
+            for_each_elem_in_js_map(js_map, f, context)?;
+            return Ok(map);
+        }
+
+        // key-valued JsObject case:
         let keys = object.__own_property_keys__(context)?;
 
         keys.into_iter()
@@ -61,4 +94,45 @@ where
             })
             .collect()
     }
+}
+
+fn for_each_elem_in_js_map<F>(js_map: JsMap, mut f: F, context: &mut Context) -> JsResult<()>
+where
+    F: FnMut(JsValue, JsValue, &mut Context) -> JsResult<()>,
+{
+    let unexp_obj_err = || {
+        JsResult::Err(
+            JsNativeError::typ()
+                .with_message("MapIterator return unexpected object")
+                .into(),
+        )
+    };
+
+    let iter = js_map.entries(context)?;
+    loop {
+        let next = iter.next(context)?;
+        let Some(iter_obj) = next.as_object() else {
+            return unexp_obj_err();
+        };
+
+        let done = iter_obj.get(js_str!("done"), context)?;
+        let Some(done) = done.as_boolean() else {
+            return unexp_obj_err();
+        };
+        if done {
+            break;
+        }
+
+        let value = iter_obj.get(js_str!("value"), context)?;
+        let Some(js_obj) = value.as_object() else {
+            return unexp_obj_err();
+        };
+        let arr = JsArray::from_object(js_obj.clone())?;
+
+        let key = arr.at(0, context)?;
+        let value = arr.at(1, context)?;
+
+        f(key, value, context)?;
+    }
+    Ok(())
 }
