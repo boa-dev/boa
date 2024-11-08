010169538b37c6c2c21126f8f19b1c013eed9178
CrazyboyQCD <yzc13697716338@gmail.com>
2024-11-08 14:14:19 +0800

    chore: change `get_typed_fn`'s name to `JsString`

--- core/engine/src/module/mod.rs
+++ core/engine/src/module/mod.rs
@@ -627,14 +627,14 @@ impl Module {
     #[inline]
     pub fn get_typed_fn<A, R>(
         &self,
-        name: JsStr<'_>,
+        name: JsString,
         context: &mut Context,
     ) -> JsResult<TypedJsFunction<A, R>>
     where
         A: crate::object::TryIntoJsArguments,
         R: crate::value::TryFromJs,
     {
-        let func = self.get_value(name, context)?;
+        let func = self.get_value(name.clone(), context)?;
         let func = func.as_function().ok_or_else(|| {
             JsNativeError::typ().with_message(format!("{name:?} is not a function"))
         })?;
