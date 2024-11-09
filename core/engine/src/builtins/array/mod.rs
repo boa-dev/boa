345bf759e41e203ec51541858b97c19afeea4d24
jasonwilliams <jase.williams@gmail.com>
2024-11-09 22:14:32 +0000

    fixup

--- core/engine/src/builtins/array/mod.rs
+++ core/engine/src/builtins/array/mod.rs
@@ -1009,7 +1009,7 @@ impl Array {
             // b. Let element be ? Get(O, ! ToString(𝔽(k))).
             let element = o.get(k, context)?;
             // c. If element is undefined, null or the array itself, let next be the empty String; otherwise, let next be ? ToString(element).
-            let next: JsString = if element.is_null_or_undefined() || &element == this {
+            let next = if element.is_null_or_undefined() || &element == this {
                 js_string!()
             } else {
                 element.to_string(context)?
