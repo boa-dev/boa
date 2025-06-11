2a2c5208290f82229337305fa3c6a4ea4bd0b6e7
Hans Larsen <hans@larsen.online>
2025-06-10 22:10:02 -0700

    reverse the polarity

--- core/engine/src/value/inner.rs
+++ core/engine/src/value/inner.rs
@@ -1,10 +1,10 @@
 //! Module implementing the operations for the inner value of a `[super::JsValue]`.
-#[cfg(feature = "legacy-jsvalue")]
+#[cfg(not(feature = "legacy-jsvalue"))]
 mod nan_boxed;
-#[cfg(feature = "legacy-jsvalue")]
+#[cfg(not(feature = "legacy-jsvalue"))]
 pub(crate) use nan_boxed::NanBoxedValue as InnerValue;
 
-#[cfg(not(feature = "legacy-jsvalue"))]
+#[cfg(feature = "legacy-jsvalue")]
 mod legacy;
-#[cfg(not(feature = "legacy-jsvalue"))]
+#[cfg(feature = "legacy-jsvalue")]
 pub(crate) use legacy::EnumBasedValue as InnerValue;
