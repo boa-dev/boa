9729a911f3509c2e3a8a12fc1b9a971c3aa573f9
randomboi404 <tanishsawant603@gmail.com>
2026-02-03 04:15:11 +0000

    Use error propagation

--- core/runtime/src/process/mod.rs
+++ core/runtime/src/process/mod.rs
@@ -1,11 +1,11 @@
-//! Boa's implementation of Node.js' `process` object.  
+//! Boa's implementation of Node.js' `process` object.    
+//!    
+//! The `process` object can be accessed from any global object.    
+//!    
+//! More information:  
+//!  - [Node.js documentation][node]    
 //!  
-//! The `process` object can be accessed from any global object.  
-//!  
-//! More information:
-//!  - [Node.js documentation][node]  
-//!
-//! [node]: https://nodejs.org/api/process.html  
+//! [node]: https://nodejs.org/api/process.html    
 
 #[cfg(test)]
 pub(crate) mod tests;
@@ -16,30 +16,36 @@ use boa_engine::{
 };
 use boa_gc::{Finalize, Trace};
 
-/// Boa's implementation of Node.js' `process` object.  
+/// Boa's implementation of Node.js' `process` object.    
 #[derive(Debug, Trace, Finalize)]
 pub struct Process;
 
 impl Process {
-    /// Name of the built-in `process` property.  
+    /// Name of the built-in `process` property.    
     pub const NAME: JsString = js_string!("process");
 
-    /// Initializes the `process` built-in object.  
-    pub fn init(context: &mut Context) -> JsObject {
+    /// Initializes the `process` built-in object.    
+    ///  
+    /// # Errors  
+    ///  
+    /// Returns a `JsError` if:  
+    /// - Setting environment variables on the `env` object fails  
+    /// - Defining the `env` property on the `process` object fails  
+    pub fn init(context: &mut Context) -> JsResult<JsObject> {
         let process = JsObject::default(context.intrinsics());
 
         // Create env object with current environment variables
         let env = JsObject::default(context.intrinsics());
         for (key, value) in std::env::vars() {
-            let _unused = env.set(
+            env.set(
                 js_string!(key),
                 JsValue::from(js_string!(value)),
                 false,
                 context,
-            );
+            )?;
         }
 
-        let _unused = process.define_property_or_throw(
+        process.define_property_or_throw(
             js_string!("env"),
             PropertyDescriptor::builder()
                 .value(env)
@@ -48,17 +54,17 @@ impl Process {
                 .configurable(true)
                 .build(),
             context,
-        );
+        )?;
 
-        process
+        Ok(process)
     }
 
-    /// Register the `process` object globally.  
-    ///  
-    /// # Errors  
-    /// This function will return an error if the property cannot be defined on the global object.  
+    /// Register the `process` object globally.    
+    ///    
+    /// # Errors    
+    /// This function will return an error if the property cannot be defined on the global object.    
     pub fn register(context: &mut Context) -> JsResult<()> {
-        let process_object = Self::init(context);
+        let process_object = Self::init(context)?;
 
         context.register_global_property(
             js_string!(Self::NAME),
