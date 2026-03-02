0ffbf682ec76c414c8c6a79587a2764983b030b8
zhuzhu <zhuzhu81998@gmail.com>
2026-03-02 11:06:52 +0000

    cast usize to u64 for comparison instead of f64

--- core/engine/src/builtins/array/from_async.rs
+++ core/engine/src/builtins/array/from_async.rs
@@ -249,7 +249,7 @@ fn from_async_iterator(
                     iterator_record,
                 } => {
                     // Inverted conditional makes for a simpler code.
-                    if (k as f64) < Number::MAX_SAFE_INTEGER {
+                    if (k as u64) < Number::MAX_SAFE_INTEGER as u64 {
                         // 2. Let Pk be ! ToString(𝔽(k)).
                         // 3. Let nextResult be ? Call(iteratorRecord.[[NextMethod]], iteratorRecord.[[Iterator]]).
                         let next_result = iterator_record.next_method().call(
