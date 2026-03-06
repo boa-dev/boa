aef28662a03aea8836aeaa66ecd88dd90d5ca770
Lokesh0224 <lokeshk20019@gmail.com>
2026-03-06 18:32:06 +0530

    style: formatting intl benchmarks

--- benches/scripts/intl/datetimeformat-construction.js
+++ benches/scripts/intl/datetimeformat-construction.js
@@ -2,9 +2,9 @@ const kIterationCount = 100;
 const locales = ["en", "de", "ja", "ar"];
 
 function main() {
-    for (let i = 0; i < kIterationCount; i++) {
-        for (const locale of locales) {
-            new Intl.DateTimeFormat(locale);
-        }
+  for (let i = 0; i < kIterationCount; i++) {
+    for (const locale of locales) {
+      new Intl.DateTimeFormat(locale);
     }
+  }
 }
--- benches/scripts/intl/numberformat-construction.js
+++ benches/scripts/intl/numberformat-construction.js
@@ -2,9 +2,9 @@ const kIterationCount = 100;
 const locales = ["en", "de", "ja", "ar"];
 
 function main() {
-    for (let i = 0; i < kIterationCount; i++) {
-        for (const locale of locales) {
-            new Intl.NumberFormat(locale);
-        }
+  for (let i = 0; i < kIterationCount; i++) {
+    for (const locale of locales) {
+      new Intl.NumberFormat(locale);
     }
+  }
 }
