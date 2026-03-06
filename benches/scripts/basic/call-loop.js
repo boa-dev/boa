00400c5624f63fc494bfad7eff8d58092584778b
Hans Larsen <hans@larsen.online>
2026-03-06 15:05:39 -0800

    formatting

--- benches/scripts/basic/call-loop.js
+++ benches/scripts/basic/call-loop.js
@@ -1,5 +1,4 @@
-function f() {
-}
+function f() {}
 
 function main() {
   for (let i = 0; i < 100_000; i++) {
--- benches/scripts/basic/nested-loop.js
+++ benches/scripts/basic/nested-loop.js
@@ -2,7 +2,6 @@ const n = 1_000;
 
 function main() {
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-    }
+    for (let j = 0; j < n; j++) {}
   }
 }
