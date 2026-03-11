6091509b39da837eeed1d5dd9a7e80635578b05d
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2026-03-11 21:42:40 +0530

    fix(builtins/function/mod,function/arguments,function/bound,object/mod): convert panics to EngineError::Panic using js_expect

--- core/engine/src/builtins/proxy/mod.rs
+++ core/engine/src/builtins/proxy/mod.rs
@@ -612,14 +612,14 @@ pub(crate) fn proxy_exotic_define_own_property(
     };
 
     // 7. Let descObj be FromPropertyDescriptor(Desc).
-    let desc_obj = OrdinaryObject::from_property_descriptor(Some(desc.clone()), context);
+    let desc_obj = OrdinaryObject::from_property_descriptor(Some(desc.clone()), context)?;
 
     // 8. Let booleanTrapResult be ! ToBoolean(? Call(trap, handler, « target, P, descObj »)).
     // 9. If booleanTrapResult is false, return false.
     if !trap
         .call(
             &handler.into(),
-            &[target.clone().into(), key.clone().into(), desc_obj],
+            &[target.clone().into(), key.clone().into()],
             context,
         )?
         .to_boolean()
