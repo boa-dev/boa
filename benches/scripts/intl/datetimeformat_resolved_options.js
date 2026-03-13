ecefdaa38663fb5993631c784df1ba53a4c3103b
Lokesh0224 <lokeshk20019@gmail.com>
2026-03-13 20:36:38 +0530

    style: format files with prettier

--- benches/scripts/intl/datetimeformat_resolved_options.js
+++ benches/scripts/intl/datetimeformat_resolved_options.js
@@ -1,12 +1,12 @@
 const kIterationCount = 1000;
 const fmt = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "full",
-    timeStyle: "short",
-    timeZone: "UTC",
+  dateStyle: "full",
+  timeStyle: "short",
+  timeZone: "UTC",
 });
 
 function main() {
   for (let i = 0; i < kIterationCount; i++) {
-      fmt.resolvedOptions();
+    fmt.resolvedOptions();
   }
 }
