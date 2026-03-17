ac8839b54a3ffc865281f47f2d69c658424f02d7
Monti-27 <msaini27004@gmail.com>
2026-03-17 15:00:11 +0530

    update url search params init

--- tests/wpt/src/lib.rs
+++ tests/wpt/src/lib.rs
@@ -1,7 +1,6 @@
 //! Integration tests running the Web Platform Tests (WPT) for the `boa_runtime` crate.
 #![allow(unused_crate_dependencies)]
 
-use boa_engine::class::Class;
 use boa_engine::interop::ContextData;
 use boa_engine::parser::source::UTF16Input;
 use boa_engine::property::Attribute;
