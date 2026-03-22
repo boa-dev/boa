1be53b36b86e0f80c80556d3985531d6ba573eb5
Kevin Ness <nekevss@gmail.com>
2026-03-22 11:39:25 -0500

    Fix tests + add more

--- core/engine/src/builtins/iterable/tests.rs
+++ core/engine/src/builtins/iterable/tests.rs
@@ -450,13 +450,37 @@ fn iterator_concat_return_result_shape() {
 #[test]
 fn iterator_includes_basic() {
     run_test_actions([
-        TestAction::run("const iter = Iterator.from([1, 3]);"),
-        TestAction::assert_eq("iter.includes(1)", true),
-        TestAction::assert_eq("iter.includes(2)", false),
-        TestAction::assert_eq("iter.includes(3)", true),
-        TestAction::assert_eq("iter.drop(1).includes(1)", false),
-        TestAction::assert_eq("iter.drop(1).includes(3)", true),
-        TestAction::assert_eq("iter.drop(2).includes(3)", false),
+        TestAction::run("const gen = () => Iterator.from([1, 3]);"),
+        TestAction::assert_eq("gen().includes(1)", true),
+        TestAction::assert_eq("gen().includes(2)", false),
+        TestAction::assert_eq("gen().includes(3)", true),
+        TestAction::assert_eq("gen().drop(1).includes(1)", false),
+        TestAction::assert_eq("gen().drop(1).includes(3)", true),
+        TestAction::assert_eq("gen().drop(2).includes(3)", false),
+        TestAction::assert_eq("gen().includes(1, 1)", false),
+        TestAction::assert_eq("gen().includes(3, 1)", true),
+        TestAction::assert_eq("gen().includes(3, 2)", false),
     ]);
 }
 
+#[test]
+fn iterator_includes_errors() {
+    run_test_actions([
+        TestAction::run("const gen = () => Iterator.from([1, 3]);"),
+        TestAction::assert_native_error(
+            "gen().includes(1, NaN)",
+            JsNativeErrorKind::Type,
+            "skippedElements must be a number"
+        ),
+        TestAction::assert_native_error(
+            "gen().includes(1, 'a string')",
+            JsNativeErrorKind::Type,
+            "skippedElements must be a number"
+        ),
+        TestAction::assert_native_error(
+            "gen().includes(1, -1)",
+            JsNativeErrorKind::Range,
+            "skippedElements must be a positive number"
+        )
+    ]);
+}
