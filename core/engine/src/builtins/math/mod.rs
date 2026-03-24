4c673a461ecd8fc88b4766a806ac7c7ed6d45c71
Hitesh <hiteshshonak1@gmail.com>
2026-03-24 11:18:47 +0530

    Update core/engine/src/builtins/math/mod.rs
    Co-authored-by: José Julián Espina <jedel0124@gmail.com>

--- core/engine/src/builtins/math/mod.rs
+++ core/engine/src/builtins/math/mod.rs
@@ -201,7 +201,7 @@ impl Math {
         let n = args.get_or_undefined(0).to_number(context)?;
         if n.is_finite() && n.abs() > ASINH_LARGE_INPUT_THRESHOLD {
             return Ok(
-                (n.signum() * (n.abs().ln() + std::f64::consts::LN_2 + (0.25 / (n * n)))).into(),
+                (n.signum() * (n.abs().ln() + std::f64::consts::LN_2 + 0.25 * n.recip() * n.recip())).into(),
             );
         }
 
